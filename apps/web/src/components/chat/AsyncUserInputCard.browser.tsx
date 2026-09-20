Anezium/synara

feat(codex): support non-blocking user question cards (#1213)
* feat(codex): support asynchronous user question cards

* refactor(chat): reuse question form for async capsules

* fix(codex): reconcile async answers with conversation history

apps/web/src/components/chat/AsyncUserInputCard.browser.tsx
@@ -1,0 +1,156 @@
+import "../../index.css";
+import { MessageId } from "@synara/contracts";
+import { describe, expect, it, vi } from "vitest";
+import { render } from "vitest-browser-react";
+import { userEvent } from "vitest/browser";
+import { AsyncUserInputCard } from "./AsyncUserInputCard";
+
+const messageId = MessageId.makeUnsafe("async-question");
+const input = {
+  questions: [
+    { title: "When does the bug happen?", options: ["On launch", "On reconnect"] },
+    { title: "Any other details?" },
+  ],
+};
+
+describe("AsyncUserInputCard", () => {
+  it("reopens after rollback even when this client has an optimistic accepted answer", async () => {
+    const questions = [{ title: "Which option?", options: ["A", "B"] }];
+    const onRespond = vi.fn().mockResolvedValue(undefined);
+    const screen = await render(
+      <AsyncUserInputCard messageId={messageId} input={{ questions }} onRespond={onRespond} />,
+    );
+    await screen.getByRole("button", { name: "1 question", exact: true }).click();
+    await screen.getByRole("button", { name: "Send answer" }).click();
+    await expect.element(screen.getByText("Answered", { exact: true })).toBeVisible();
+    await screen.rerender(
+      <AsyncUserInputCard
+        messageId={messageId}
+        input={{ questions, responseSequence: 10 }}
+        onRespond={onRespond}
+      />,
+    );
+    await expect.element(screen.getByText("Answered", { exact: true })).not.toBeInTheDocument();
+    await screen.getByRole("button", { name: /B/ }).click();
+    await screen.getByRole("button", { name: "Send answer" }).click();
+    expect(onRespond).toHaveBeenLastCalledWith(messageId, ["B"]);
+  });
+  it("requires an explicit submission, allows free text, and prevents double clicks", async () => {
+    let accept!: () => void;
+    const onRespond = vi.fn(
+      () =>
+        new Promise<void>((resolve) => {
+          accept = resolve;
+        }),
+    );
+    const screen = await render(
+      <div className="max-w-xl p-4">
+        <AsyncUserInputCard messageId={messageId} input={input} onRespond={onRespond} />
+        <textarea aria-label="Conversation composer" />
+      </div>,
+    );
+    const capsule = screen.getByRole("button", { name: "2 questions", exact: true });
+    await expect.element(capsule).toHaveAttribute("aria-expanded", "false");
+    await expect.element(screen.getByRole("form")).not.toBeInTheDocument();
+    await capsule.click();
+    await expect
+      .element(screen.getByRole("button", { name: /On launch/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    expect(onRespond).not.toHaveBeenCalled();
+    await screen.getByRole("button", { name: /On reconnect/ }).click();
+    await screen.getByRole("button", { name: "Next", exact: true }).click();
+    await expect.element(screen.getByRole("button", { name: "Send answer" })).toBeDisabled();
+    await screen
+      .getByRole("textbox", { name: "Answer: Any other details?" })
+      .fill("Only after waking from sleep");
+    await screen
+      .getByRole("textbox", { name: "Conversation composer" })
+      .fill("Keep checking the logs.");
+    await capsule.click();
+    await expect.element(capsule).toHaveAttribute("aria-expanded", "false");
+    await capsule.click();
+    await expect
+      .element(screen.getByRole("textbox", { name: "Answer: Any other details?" }))
+      .toHaveValue("Only after waking from sleep");
+    await screen.getByRole("button", { name: "Previous question" }).click();
+    await expect
+      .element(screen.getByRole("button", { name: /On reconnect/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    await screen.getByRole("button", { name: "Next", exact: true }).click();
+    await screen.getByRole("button", { name: "Send answer" }).click();
+    await expect.element(screen.getByRole("button", { name: "Submitting…" })).toBeDisabled();
+    expect(onRespond).toHaveBeenCalledExactlyOnceWith(messageId, [
+      "On reconnect",
+      "Only after waking from sleep",
+    ]);
+    accept();
+    await expect.element(screen.getByText("Answered", { exact: true })).toBeVisible();
+    await expect
+      .element(screen.getByRole("textbox", { name: "Conversation composer" }))
+      .toHaveValue("Keep checking the logs.");
+  });
+
+  it("restores an answered card and keeps rejected submissions editable", async () => {
+    const onRespond = vi.fn().mockRejectedValue(new Error("Connection interrupted"));
+    const screen = await render(
+      <AsyncUserInputCard
+        messageId={messageId}
+        input={{ questions: [{ title: "More details?" }] }}
+        onRespond={onRespond}
+      />,
+    );
+    await screen.getByRole("button", { name: "1 question", exact: true }).click();
+    await screen.getByRole("textbox").fill("Custom answer");
+    await screen.getByRole("button", { name: "Send answer" }).click();
+    await expect.element(screen.getByRole("alert")).toHaveTextContent("Connection interrupted");
+    await expect.element(screen.getByRole("textbox")).toHaveValue("Custom answer");
+    await screen.rerender(
+      <AsyncUserInputCard
+        messageId={messageId}
+        input={{
+          questions: [{ title: "More details?" }],
+          response: { messageId: MessageId.makeUnsafe("answer"), answers: ["Answered elsewhere"] },
+        }}
+        onRespond={onRespond}
+      />,
+    );
+    await expect.element(screen.getByText("Answered elsewhere")).toBeVisible();
+    await expect
+      .element(screen.getByRole("button", { name: "Send answer" }))
+      .not.toBeInTheDocument();
+  });
+
+  it("keeps shortcuts local to the opened question and never submits a suggested choice", async () => {
+    const onRespond = vi.fn();
+    const screen = await render(
+      <div>
+        <AsyncUserInputCard
+          messageId={messageId}
+          input={{ questions: [{ title: "First?", options: ["Alpha", "Beta"] }] }}
+          onRespond={onRespond}
+        />
+        <AsyncUserInputCard
+          messageId={MessageId.makeUnsafe("second")}
+          input={{ questions: [{ title: "Second?", options: ["Gamma", "Delta"] }] }}
+          onRespond={onRespond}
+        />
+        <textarea aria-label="Conversation composer" />
+      </div>,
+    );
+    await screen.getByRole("button", { name: "1 question", exact: true }).nth(0).click();
+    await screen.getByRole("button", { name: "1 question", exact: true }).nth(1).click();
+    await screen.getByRole("button", { name: /Alpha/ }).click();
+    await userEvent.keyboard("2");
+    await expect
+      .element(screen.getByRole("button", { name: /Beta/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    await expect
+      .element(screen.getByRole("button", { name: /Gamma/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    await screen.getByRole("textbox", { name: "Conversation composer" }).fill("123");
+    await expect
+      .element(screen.getByRole("button", { name: /Beta/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    expect(onRespond).not.toHaveBeenCalled();
+  });
+});

apps/web/src/storeNormalization.ts
@@ -14,6 +14,7 @@ import {
   type TurnId,
 } from "@synara/contracts";
 import { resolveThreadBranchRegressionGuard } from "@synara/shared/git";
+import { mergeAsyncUserInput } from "@synara/shared/asyncUserInput";
 import { normalizeModelSlug } from "@synara/shared/model";
 import { deriveThreadSummaryMetadata } from "@synara/shared/threadSummary";
 
@@ -550,10 +551,12 @@ export function normalizeChatMessage(
   const previousSkills = previous?.skills ?? [];
   const previousMentions = previous?.mentions ?? [];
   const completedAt = incoming.streaming ? undefined : incoming.updatedAt;
+  const asyncUserInput = mergeAsyncUserInput(previous?.asyncUserInput, incoming.asyncUserInput);
   if (
     previous &&
     previous.role === incoming.role &&
     previous.text === incoming.text &&
+    previous.asyncUserInput === asyncUserInput &&
     previous.dispatchMode === incoming.dispatchMode &&
     previous.dispatchOrigin === incoming.dispatchOrigin &&
     previous.startsNewTurn === incoming.startsNewTurn &&
@@ -574,6 +577,7 @@ export function normalizeChatMessage(
     id: incoming.id,
     role: incoming.role,
     text: incoming.text,
+    ...(asyncUserInput ? { asyncUserInput } : {}),
     ...(incoming.textSegments !== undefined && incoming.textSegments.length > 0
       ? { textSegments: [...incoming.textSegments] }
       : {}),
@@ -639,6 +643,7 @@ function readModelMessageFromChatMessage(
     id: message.id,
     role: message.role,
     text: message.text,
+    ...(message.asyncUserInput ? { asyncUserInput: message.asyncUserInput } : {}),
     ...(message.dispatchMode ? { dispatchMode: message.dispatchMode } : {}),
     ...(message.dispatchOrigin ? { dispatchOrigin: message.dispatchOrigin } : {}),
     ...(message.startsNewTurn !== undefined ? { startsNewTurn: message.startsNewTurn } : {}),
@@ -781,6 +786,10 @@ function mergeReadModelMessagesWithLiveHotPath(
       dispatchMode: previousMessage.dispatchMode ?? incomingMessage.dispatchMode,
       dispatchOrigin: incomingMessage.dispatchOrigin ?? previousMessage.dispatchOrigin,
       startsNewTurn: incomingMessage.startsNewTurn ?? previousMessage.startsNewTurn,
+      asyncUserInput: mergeAsyncUserInput(
+        previousMessage.asyncUserInput,
+        incomingMessage.asyncUserInput,
+      ),
       turnId: previousMessage.turnId ?? incomingMessage.turnId ?? null,
       source: previousMessage.source ?? incomingMessage.source ?? "native",
       streaming: previousMessage.streaming,

packages/shared/src/asyncUserInput.test.ts
@@ -1,0 +1,39 @@
+import { MessageId } from "@synara/contracts";
+import { expect, it } from "vitest";
+import { clearRemovedAsyncUserInputResponses, mergeAsyncUserInput } from "./asyncUserInput";
+
+it("only clears answers removed by a history edit", () => {
+  const questions = [{ title: "Which option?" }];
+  const answered = {
+    questions,
+    response: { messageId: MessageId.makeUnsafe("answer"), answers: ["A"] },
+    responseSequence: 5,
+  };
+  const messages = [{ id: "question", asyncUserInput: answered }];
+  expect(
+    clearRemovedAsyncUserInputResponses(messages, new Set(["question", "answer"]), 10)[0],
+  ).toBe(messages[0]);
+  expect(
+    clearRemovedAsyncUserInputResponses(messages, new Set(["question"]), 10)[0]?.asyncUserInput,
+  ).toEqual({ questions, responseSequence: 10 });
+});
+
+it("orders answer and rollback metadata independently of message text", () => {
+  const pending = { questions: [{ title: "Which option?" }] };
+  const answered = {
+    ...pending,
+    response: { messageId: MessageId.makeUnsafe("answer"), answers: ["A"] },
+    responseSequence: 5,
+  };
+  const reopened = { ...pending, responseSequence: 10 };
+  expect(mergeAsyncUserInput(answered, pending)).toBe(answered);
+  expect(mergeAsyncUserInput(answered, reopened)).toBe(reopened);
+  expect(mergeAsyncUserInput(reopened, answered)).toBe(reopened);
+  const replacement = {
+    ...answered,
+    response: { ...answered.response, answers: ["B"] },
+    responseSequence: 15,
+  };
+  expect(mergeAsyncUserInput(reopened, replacement)).toBe(replacement);
+  expect(mergeAsyncUserInput(replacement, answered)).toBe(replacement);
+});

packages/shared/src/asyncUserInput.ts
@@ -1,0 +1,49 @@
+import type { AsyncUserInput, AsyncUserInputQuestions } from "@synara/contracts";
+
+export const ASYNC_USER_INPUT_ALREADY_ANSWERED =
+  "This asynchronous question has already been answered.";
+
+export function mergeAsyncUserInput(
+  previous: AsyncUserInput,
+  incoming: AsyncUserInput | undefined,
+): AsyncUserInput;
+export function mergeAsyncUserInput(
+  previous: AsyncUserInput | undefined,
+  incoming: AsyncUserInput | undefined,
+): AsyncUserInput | undefined;
+export function mergeAsyncUserInput(
+  previous: AsyncUserInput | undefined,
+  incoming: AsyncUserInput | undefined,
+): AsyncUserInput | undefined {
+  if (!previous) return incoming;
+  if (!incoming) return previous;
+  const previousSequence = previous.responseSequence ?? 0;
+  const incomingSequence = incoming.responseSequence ?? 0;
+  if (previousSequence > incomingSequence) return previous;
+  if (incomingSequence > previousSequence) return incoming;
+  // Legacy snapshots have no sequence; retain an already accepted answer.
+  return previous.response ? previous : incoming;
+}
+
+// Only use on deliberate history removal, before applying transcript size caps.
+export function clearRemovedAsyncUserInputResponses<
+  T extends { readonly asyncUserInput?: AsyncUserInput | undefined },
+>(messages: ReadonlyArray<T>, retainedMessageIds: ReadonlySet<string>, sequence: number): T[] {
+  return messages.map((message) => {
+    const input = message.asyncUserInput;
+    if (!input?.response || retainedMessageIds.has(input.response.messageId)) return message;
+    return {
+      ...message,
+      asyncUserInput: { questions: input.questions, responseSequence: sequence },
+    };
+  });
+}
+
+export function formatAsyncUserInputResponse(
+  questions: AsyncUserInputQuestions,
+  answers: readonly string[],
+): string {
+  return questions
+    .map((question, index) => `${question.title}\n${answers[index] ?? ""}`)
+    .join("\n\n");
+}

redxzeta/forkara

apps/web/src/components/chat/AsyncUserInputCard.browser.tsx
@@ -1,0 +1,156 @@
+import "../../index.css";
+import { MessageId } from "@forkara/contracts";
+import { describe, expect, it, vi } from "vitest";
+import { render } from "vitest-browser-react";
+import { userEvent } from "vitest/browser";
+import { AsyncUserInputCard } from "./AsyncUserInputCard";
+
+const messageId = MessageId.makeUnsafe("async-question");
+const input = {
+  questions: [
+    { title: "When does the bug happen?", options: ["On launch", "On reconnect"] },
+    { title: "Any other details?" },
+  ],
+};
+
+describe("AsyncUserInputCard", () => {
+  it("reopens after rollback even when this client has an optimistic accepted answer", async () => {
+    const questions = [{ title: "Which option?", options: ["A", "B"] }];
+    const onRespond = vi.fn().mockResolvedValue(undefined);
+    const screen = await render(
+      <AsyncUserInputCard messageId={messageId} input={{ questions }} onRespond={onRespond} />,
+    );
+    await screen.getByRole("button", { name: "1 question", exact: true }).click();
+    await screen.getByRole("button", { name: "Send answer" }).click();
+    await expect.element(screen.getByText("Answered", { exact: true })).toBeVisible();
+    await screen.rerender(
+      <AsyncUserInputCard
+        messageId={messageId}
+        input={{ questions, responseSequence: 10 }}
+        onRespond={onRespond}
+      />,
+    );
+    await expect.element(screen.getByText("Answered", { exact: true })).not.toBeInTheDocument();
+    await screen.getByRole("button", { name: /B/ }).click();
+    await screen.getByRole("button", { name: "Send answer" }).click();
+    expect(onRespond).toHaveBeenLastCalledWith(messageId, ["B"]);
+  });
+  it("requires an explicit submission, allows free text, and prevents double clicks", async () => {
+    let accept!: () => void;
+    const onRespond = vi.fn(
+      () =>
+        new Promise<void>((resolve) => {
+          accept = resolve;
+        }),
+    );
+    const screen = await render(
+      <div className="max-w-xl p-4">
+        <AsyncUserInputCard messageId={messageId} input={input} onRespond={onRespond} />
+        <textarea aria-label="Conversation composer" />
+      </div>,
+    );
+    const capsule = screen.getByRole("button", { name: "2 questions", exact: true });
+    await expect.element(capsule).toHaveAttribute("aria-expanded", "false");
+    await expect.element(screen.getByRole("form")).not.toBeInTheDocument();
+    await capsule.click();
+    await expect
+      .element(screen.getByRole("button", { name: /On launch/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    expect(onRespond).not.toHaveBeenCalled();
+    await screen.getByRole("button", { name: /On reconnect/ }).click();
+    await screen.getByRole("button", { name: "Next", exact: true }).click();
+    await expect.element(screen.getByRole("button", { name: "Send answer" })).toBeDisabled();
+    await screen
+      .getByRole("textbox", { name: "Answer: Any other details?" })
+      .fill("Only after waking from sleep");
+    await screen
+      .getByRole("textbox", { name: "Conversation composer" })
+      .fill("Keep checking the logs.");
+    await capsule.click();
+    await expect.element(capsule).toHaveAttribute("aria-expanded", "false");
+    await capsule.click();
+    await expect
+      .element(screen.getByRole("textbox", { name: "Answer: Any other details?" }))
+      .toHaveValue("Only after waking from sleep");
+    await screen.getByRole("button", { name: "Previous question" }).click();
+    await expect
+      .element(screen.getByRole("button", { name: /On reconnect/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    await screen.getByRole("button", { name: "Next", exact: true }).click();
+    await screen.getByRole("button", { name: "Send answer" }).click();
+    await expect.element(screen.getByRole("button", { name: "Submitting…" })).toBeDisabled();
+    expect(onRespond).toHaveBeenCalledExactlyOnceWith(messageId, [
+      "On reconnect",
+      "Only after waking from sleep",
+    ]);
+    accept();
+    await expect.element(screen.getByText("Answered", { exact: true })).toBeVisible();
+    await expect
+      .element(screen.getByRole("textbox", { name: "Conversation composer" }))
+      .toHaveValue("Keep checking the logs.");
+  });
+
+  it("restores an answered card and keeps rejected submissions editable", async () => {
+    const onRespond = vi.fn().mockRejectedValue(new Error("Connection interrupted"));
+    const screen = await render(
+      <AsyncUserInputCard
+        messageId={messageId}
+        input={{ questions: [{ title: "More details?" }] }}
+        onRespond={onRespond}
+      />,
+    );
+    await screen.getByRole("button", { name: "1 question", exact: true }).click();
+    await screen.getByRole("textbox").fill("Custom answer");
+    await screen.getByRole("button", { name: "Send answer" }).click();
+    await expect.element(screen.getByRole("alert")).toHaveTextContent("Connection interrupted");
+    await expect.element(screen.getByRole("textbox")).toHaveValue("Custom answer");
+    await screen.rerender(
+      <AsyncUserInputCard
+        messageId={messageId}
+        input={{
+          questions: [{ title: "More details?" }],
+          response: { messageId: MessageId.makeUnsafe("answer"), answers: ["Answered elsewhere"] },
+        }}
+        onRespond={onRespond}
+      />,
+    );
+    await expect.element(screen.getByText("Answered elsewhere")).toBeVisible();
+    await expect
+      .element(screen.getByRole("button", { name: "Send answer" }))
+      .not.toBeInTheDocument();
+  });
+
+  it("keeps shortcuts local to the opened question and never submits a suggested choice", async () => {
+    const onRespond = vi.fn();
+    const screen = await render(
+      <div>
+        <AsyncUserInputCard
+          messageId={messageId}
+          input={{ questions: [{ title: "First?", options: ["Alpha", "Beta"] }] }}
+          onRespond={onRespond}
+        />
+        <AsyncUserInputCard
+          messageId={MessageId.makeUnsafe("second")}
+          input={{ questions: [{ title: "Second?", options: ["Gamma", "Delta"] }] }}
+          onRespond={onRespond}
+        />
+        <textarea aria-label="Conversation composer" />
+      </div>,
+    );
+    await screen.getByRole("button", { name: "1 question", exact: true }).nth(0).click();
+    await screen.getByRole("button", { name: "1 question", exact: true }).nth(1).click();
+    await screen.getByRole("button", { name: /Alpha/ }).click();
+    await userEvent.keyboard("2");
+    await expect
+      .element(screen.getByRole("button", { name: /Beta/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    await expect
+      .element(screen.getByRole("button", { name: /Gamma/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    await screen.getByRole("textbox", { name: "Conversation composer" }).fill("123");
+    await expect
+      .element(screen.getByRole("button", { name: /Beta/ }))
+      .toHaveAttribute("aria-pressed", "true");
+    expect(onRespond).not.toHaveBeenCalled();
+  });
+});

apps/web/src/storeNormalization.ts
@@ -14,6 +14,7 @@ import {
   type TurnId,
 } from "@forkara/contracts";
 import { resolveThreadBranchRegressionGuard } from "@forkara/shared/git";
+import { mergeAsyncUserInput } from "@forkara/shared/asyncUserInput";
 import { normalizeModelSlug } from "@forkara/shared/model";
 import { deriveThreadSummaryMetadata } from "@forkara/shared/threadSummary";
 
@@ -531,10 +532,12 @@ export function normalizeChatMessage(
   const previousSkills = previous?.skills ?? [];
   const previousMentions = previous?.mentions ?? [];
   const completedAt = incoming.streaming ? undefined : incoming.updatedAt;
+  const asyncUserInput = mergeAsyncUserInput(previous?.asyncUserInput, incoming.asyncUserInput);
   if (
     previous &&
     previous.role === incoming.role &&
     previous.text === incoming.text &&
+    previous.asyncUserInput === asyncUserInput &&
     previous.dispatchMode === incoming.dispatchMode &&
     previous.dispatchOrigin === incoming.dispatchOrigin &&
     previous.turnId === incoming.turnId &&
@@ -554,6 +557,7 @@ export function normalizeChatMessage(
     id: incoming.id,
     role: incoming.role,
     text: incoming.text,
+    ...(asyncUserInput ? { asyncUserInput } : {}),
     ...(incoming.textSegments !== undefined && incoming.textSegments.length > 0
       ? { textSegments: [...incoming.textSegments] }
       : {}),
@@ -618,6 +622,7 @@ function readModelMessageFromChatMessage(
     id: message.id,
     role: message.role,
     text: message.text,
+    ...(message.asyncUserInput ? { asyncUserInput: message.asyncUserInput } : {}),
     ...(message.dispatchMode ? { dispatchMode: message.dispatchMode } : {}),
     ...(message.dispatchOrigin ? { dispatchOrigin: message.dispatchOrigin } : {}),
     turnId: message.turnId ?? null,
@@ -758,6 +763,11 @@ function mergeReadModelMessagesWithLiveHotPath(
       text: previousMessage.text,
       dispatchMode: previousMessage.dispatchMode ?? incomingMessage.dispatchMode,
       dispatchOrigin: incomingMessage.dispatchOrigin ?? previousMessage.dispatchOrigin,
+      startsNewTurn: incomingMessage.startsNewTurn ?? previousMessage.startsNewTurn,
+      asyncUserInput: mergeAsyncUserInput(
+        previousMessage.asyncUserInput,
+        incomingMessage.asyncUserInput,
+      ),
       turnId: previousMessage.turnId ?? incomingMessage.turnId ?? null,
       source: previousMessage.source ?? incomingMessage.source ?? "native",
       streaming: previousMessage.streaming,

packages/shared/src/asyncUserInput.test.ts
@@ -1,0 +1,39 @@
+import { MessageId } from "@forkara/contracts";
+import { expect, it } from "vitest";
+import { clearRemovedAsyncUserInputResponses, mergeAsyncUserInput } from "./asyncUserInput";
+
+it("only clears answers removed by a history edit", () => {
+  const questions = [{ title: "Which option?" }];
+  const answered = {
+    questions,
+    response: { messageId: MessageId.makeUnsafe("answer"), answers: ["A"] },
+    responseSequence: 5,
+  };
+  const messages = [{ id: "question", asyncUserInput: answered }];
+  expect(
+    clearRemovedAsyncUserInputResponses(messages, new Set(["question", "answer"]), 10)[0],
+  ).toBe(messages[0]);
+  expect(
+    clearRemovedAsyncUserInputResponses(messages, new Set(["question"]), 10)[0]?.asyncUserInput,
+  ).toEqual({ questions, responseSequence: 10 });
+});
+
+it("orders answer and rollback metadata independently of message text", () => {
+  const pending = { questions: [{ title: "Which option?" }] };
+  const answered = {
+    ...pending,
+    response: { messageId: MessageId.makeUnsafe("answer"), answers: ["A"] },
+    responseSequence: 5,
+  };
+  const reopened = { ...pending, responseSequence: 10 };
+  expect(mergeAsyncUserInput(answered, pending)).toBe(answered);
+  expect(mergeAsyncUserInput(answered, reopened)).toBe(reopened);
+  expect(mergeAsyncUserInput(reopened, answered)).toBe(reopened);
+  const replacement = {
+    ...answered,
+    response: { ...answered.response, answers: ["B"] },
+    responseSequence: 15,
+  };
+  expect(mergeAsyncUserInput(reopened, replacement)).toBe(replacement);
+  expect(mergeAsyncUserInput(replacement, answered)).toBe(replacement);
+});

packages/shared/src/asyncUserInput.ts
@@ -1,0 +1,49 @@
+import type { AsyncUserInput, AsyncUserInputQuestions } from "@forkara/contracts";
+
+export const ASYNC_USER_INPUT_ALREADY_ANSWERED =
+  "This asynchronous question has already been answered.";
+
+export function mergeAsyncUserInput(
+  previous: AsyncUserInput,
+  incoming: AsyncUserInput | undefined,
+): AsyncUserInput;
+export function mergeAsyncUserInput(
+  previous: AsyncUserInput | undefined,
+  incoming: AsyncUserInput | undefined,
+): AsyncUserInput | undefined;
+export function mergeAsyncUserInput(
+  previous: AsyncUserInput | undefined,
+  incoming: AsyncUserInput | undefined,
+): AsyncUserInput | undefined {
+  if (!previous) return incoming;
+  if (!incoming) return previous;
+  const previousSequence = previous.responseSequence ?? 0;
+  const incomingSequence = incoming.responseSequence ?? 0;
+  if (previousSequence > incomingSequence) return previous;
+  if (incomingSequence > previousSequence) return incoming;
+  // Legacy snapshots have no sequence; retain an already accepted answer.
+  return previous.response ? previous : incoming;
+}
+
+// Only use on deliberate history removal, before applying transcript size caps.
+export function clearRemovedAsyncUserInputResponses<
+  T extends { readonly asyncUserInput?: AsyncUserInput | undefined },
+>(messages: ReadonlyArray<T>, retainedMessageIds: ReadonlySet<string>, sequence: number): T[] {
+  return messages.map((message) => {
+    const input = message.asyncUserInput;
+    if (!input?.response || retainedMessageIds.has(input.response.messageId)) return message;
+    return {
+      ...message,
+      asyncUserInput: { questions: input.questions, responseSequence: sequence },
+    };
+  });
+}
+
+export function formatAsyncUserInputResponse(
+  questions: AsyncUserInputQuestions,
+  answers: readonly string[],
+): string {
+  return questions
+    .map((question, index) => `${question.title}\n${answers[index] ?? ""}`)
+    .join("\n\n");
+}