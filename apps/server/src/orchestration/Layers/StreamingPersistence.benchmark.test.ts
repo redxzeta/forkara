Emanuele-web04/synara

Store streamed assistant text as chunks and tighten provider memory/cleanup
Streaming assistant deltas rewrote the whole accumulated message row twice per
token chunk (projection_thread_messages.text and message_text_segments.text),
which is O(length²/chunk) of WAL per message: measured through the real engine,
a 200 KB message in 40-byte chunks wrote 1.3 GiB. Migration 100 adds an
append-only message_text_chunks table; deltas insert one chunk row, completion
materializes the body once, and every reader assembles text from chunks
(200 KB → ~400 MiB; the per-delta floor is unchanged and addressed separately).

Also in this change:
- ProjectionSnapshotQuery ranks identities before loading bodies and only joins
  segments for the retained window.
- ProviderService retries failed idle teardown instead of recording a stopped
  binding; adapters always cross their idempotent cleanup barrier.
- Claude retains ownership of a process whose installation failed.
- Shared ACP notification dispatcher bounds SDK callbacks by count and bytes;
  ACP adapters return copied readThread/rollback snapshots.
- OpenCode drops the unused per-turn mirror, keeps SHA-256 fingerprints instead
  of JSON copies of every part, and releases part state on removal.
- Pi keeps one snapshot per tool call instead of every cumulative update.
- Callback ingress reserves capacity for resource-settling events and releases
  buffers on owner shutdown.
- Dock terminals and detached xterm runtimes are disposed with their host.
- docs/performance/2026-09-09-memory-crash records the audits, reviews,
  measurements and reproduction scripts.

Co-Authored-By: Claude Fable 5.1 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01X6tfmKKiepVr67f5RtPtZp

apps/server/src/orchestration/Layers/StreamingPersistence.benchmark.test.ts
@@ -1,0 +1,198 @@
+import { mkdtemp, readFile, rm, stat, writeFile } from "node:fs/promises";
+import { createHash } from "node:crypto";
+import { tmpdir, platform, release, totalmem, cpus } from "node:os";
+import { join } from "node:path";
+import { monitorEventLoopDelay } from "node:perf_hooks";
+import { setImmediate } from "node:timers/promises";
+import * as NodeServices from "@effect/platform-node/NodeServices";
+import {
+  CommandId,
+  DEFAULT_PROVIDER_INTERACTION_MODE,
+  MessageId,
+  ProjectId,
+  ThreadId,
+} from "@synara/contracts";
+import { Effect, Layer, ManagedRuntime } from "effect";
+import * as SqlClient from "effect/unstable/sql/SqlClient";
+import { expect, it } from "vitest";
+
+import { ServerConfig } from "../../config.ts";
+import { makeSqlitePersistenceLive } from "../../persistence/Layers/Sqlite.ts";
+import { OrchestrationEventStoreLive } from "../../persistence/Layers/OrchestrationEventStore.ts";
+import { OrchestrationCommandReceiptRepositoryLive } from "../../persistence/Layers/OrchestrationCommandReceipts.ts";
+import { ProjectionSnapshotQuery } from "../Services/ProjectionSnapshotQuery.ts";
+import { OrchestrationEngineService } from "../Services/OrchestrationEngine.ts";
+import { OrchestrationEngineLive } from "./OrchestrationEngine.ts";
+import { OrchestrationProjectionPipelineLive } from "./ProjectionPipeline.ts";
+import { OrchestrationProjectionSnapshotQueryLive } from "./ProjectionSnapshotQuery.ts";
+
+// Opt-in only. Each invocation uses a fresh process and a disposable database.
+it.skipIf(!process.env.SYNARA_STREAMING_BENCHMARK_OUTPUT)(
+  "measures the production streaming engine",
+  async () => {
+    const output = process.env.SYNARA_STREAMING_BENCHMARK_OUTPUT!;
+    const messageBytes = Number(process.env.SYNARA_STREAMING_BENCHMARK_BYTES ?? 200_000);
+    const chunkBytes = 40;
+    const threadCount = Number(process.env.SYNARA_STREAMING_BENCHMARK_THREADS ?? 1);
+    const dir = await mkdtemp(join(tmpdir(), "synara-streaming-benchmark-"));
+    const dbPath = join(dir, "state.sqlite");
+    const runtime = ManagedRuntime.make(
+      OrchestrationEngineLive.pipe(
+        Layer.provide(OrchestrationProjectionPipelineLive),
+        Layer.provideMerge(OrchestrationProjectionSnapshotQueryLive),
+        Layer.provide(OrchestrationEventStoreLive),
+        Layer.provide(OrchestrationCommandReceiptRepositoryLive),
+        Layer.provideMerge(makeSqlitePersistenceLive(dbPath)),
+        Layer.provideMerge(ServerConfig.layerTest(process.cwd(), dir)),
+        Layer.provideMerge(NodeServices.layer),
+      ),
+    );
+    const delay = monitorEventLoopDelay({ resolution: 10 });
+    try {
+      const engine = await runtime.runPromise(Effect.service(OrchestrationEngineService));
+      const snapshots = await runtime.runPromise(Effect.service(ProjectionSnapshotQuery));
+      const sql = await runtime.runPromise(Effect.service(SqlClient.SqlClient));
+      const createdAt = "2026-09-10T12:00:00.000Z";
+      const projectId = ProjectId.makeUnsafe("benchmark-project");
+      await runtime.runPromise(
+        engine.dispatch({
+          type: "project.create",
+          commandId: CommandId.makeUnsafe("project"),
+          projectId,
+          title: "Benchmark",
+          workspaceRoot: dir,
+          defaultModelSelection: null,
+          createdAt,
+        }),
+      );
+      const threads = Array.from({ length: threadCount }, (_, index) =>
+        ThreadId.makeUnsafe(`benchmark-thread-${index}`),
+      );
+      for (const threadId of threads) {
+        await runtime.runPromise(
+          engine.dispatch({
+            type: "thread.create",
+            commandId: CommandId.makeUnsafe(threadId),
+            threadId,
+            projectId,
+            title: "Benchmark",
+            modelSelection: { provider: "codex", model: "gpt-5-codex" },
+            interactionMode: DEFAULT_PROVIDER_INTERACTION_MODE,
+            runtimeMode: "approval-required",
+            branch: null,
+            worktreePath: null,
+            createdAt,
+          }),
+        );
+      }
+      await runtime.runPromise(sql`PRAGMA wal_autocheckpoint = 0`);
+      await runtime.runPromise(sql`PRAGMA wal_checkpoint(TRUNCATE)`);
+      const memoryBefore = process.memoryUsage();
+      let peakRss = memoryBefore.rss;
+      let peakHeapUsed = memoryBefore.heapUsed;
+      delay.enable();
+      await setImmediate();
+      const started = performance.now();
+      for (let offset = 0; offset < messageBytes; offset += chunkBytes) {
+        for (const threadId of threads) {
+          await runtime.runPromise(
+            engine.dispatch({
+              type: "thread.message.assistant.delta",
+              commandId: CommandId.makeUnsafe(`${threadId}-${offset}`),
+              threadId,
+              messageId: MessageId.makeUnsafe("benchmark-message"),
+              delta: "x".repeat(Math.min(chunkBytes, messageBytes - offset)),
+              createdAt,
+            }),
+          );
+        }
+        if ((offset / chunkBytes) % 100 === 0) {
+          const memory = process.memoryUsage();
+          peakRss = Math.max(peakRss, memory.rss);
+          peakHeapUsed = Math.max(peakHeapUsed, memory.heapUsed);
+          // Yield every 100 chunks so timer delay has a reproducible sampling window.
+          await setImmediate();
+        }
+      }
+      await setImmediate();
+      const streamMs = performance.now() - started;
+      delay.disable();
+      const streamingWalBytes = (await stat(`${dbPath}-wal`)).size;
+      const readStarted = performance.now();
+      const streaming = await runtime.runPromise(snapshots.getSnapshot());
+      const streamingReadMs = performance.now() - readStarted;
+      expect(streaming.threads).toHaveLength(threadCount);
+      for (const thread of streaming.threads)
+        expect(thread.messages[0]?.text).toBe("x".repeat(messageBytes));
+      const completionStarted = performance.now();
+      for (const threadId of threads) {
+        await runtime.runPromise(
+          engine.dispatch({
+            type: "thread.message.assistant.complete",
+            commandId: CommandId.makeUnsafe(`${threadId}-complete`),
+            threadId,
+            messageId: MessageId.makeUnsafe("benchmark-message"),
+            createdAt,
+          }),
+        );
+      }
+      const completionMs = performance.now() - completionStarted;
+      const completed = await runtime.runPromise(snapshots.getSnapshot());
+      expect(completed.threads).toHaveLength(threadCount);
+      for (const thread of completed.threads)
+        expect(thread.messages[0]?.text).toBe("x".repeat(messageBytes));
+      const finalWalBytes = (await stat(`${dbPath}-wal`)).size;
+      const hashes: Record<string, string> = {};
+      for (const path of [
+        "src/orchestration/Layers/ProjectionPipeline.ts",
+        "src/persistence/Layers/ProjectionThreadMessages.ts",
+        "src/persistence/Migrations.ts",
+        "src/orchestration/Layers/OrchestrationEngine.ts",
+        "src/persistence/messageTextChunks.ts",
+      ])
+        hashes[path] = createHash("sha256")
+          .update(await readFile(path))
+          .digest("hex");
+      await writeFile(
+        output,
+        JSON.stringify(
+          {
+            environment: {
+              node: process.version,
+              os: `${platform()} ${release()}`,
+              cpu: cpus()[0]?.model,
+              totalMemoryBytes: totalmem(),
+              sqlite: await runtime.runPromise(sql`SELECT sqlite_version() AS version`),
+            },
+            workload: {
+              messageBytes,
+              chunkBytes,
+              threadCount,
+              transactions: Math.ceil(messageBytes / chunkBytes) * threadCount,
+              checkpoint: "disabled during measurement; setup WAL truncated",
+              timerYieldEveryChunks: 100,
+            },
+            streamingWalBytes,
+            finalWalBytes,
+            streamMs,
+            streamingReadMs,
+            completionMs,
+            eventLoopDelayMs: { max: delay.max / 1e6, p99: delay.percentile(99) / 1e6 },
+            memoryBefore,
+            memoryAfter: process.memoryUsage(),
+            sampledPeakRss: peakRss,
+            sampledPeakHeapUsed: peakHeapUsed,
+            hashes,
+          },
+          null,
+          2,
+        ),
+      );
+    } finally {
+      delay.disable();
+      await runtime.dispose();
+      await rm(dir, { recursive: true, force: true });
+    }
+  },
+  180_000,
+);

apps/server/src/orchestration/Layers/ThreadDeletionReactor.ts
@@ -1,5 +1,6 @@
 import { ThreadId, type OrchestrationEvent } from "@synara/contracts";
 import { makeDrainableWorker, startDrainableWorkerProducers } from "@synara/shared/DrainableWorker";
+import { terminalScopeIdsForThread } from "@synara/shared/terminalThreads";
 import { Cause, Effect, Layer, Option, Stream } from "effect";
 
 import { ServerConfig } from "../../config";
@@ -8,7 +9,7 @@ import { GitCore } from "../../git/Services/GitCore";
 import { pruneProjectedArchivedManagedWorktrees } from "../../managedWorktrees";
 import { ProfileStatsArchive } from "../../profileStatsArchive";
 import { ProviderService } from "../../provider/Services/ProviderService";
-import { TerminalManager } from "../../terminal/Services/Manager";
+import { TerminalManager, type TerminalManagerShape } from "../../terminal/Services/Manager";
 import { THREAD_RETENTION_COMMAND_ID_PREFIX } from "../../threadRetention";
 import { OrchestrationEngineService } from "../Services/OrchestrationEngine";
 import { ProjectionSnapshotQuery } from "../Services/ProjectionSnapshotQuery";
@@ -81,6 +82,26 @@ export const detachThreadDevice = (threadId: ThreadId) =>
       ),
     ),
   );
+
+export const closeThreadTerminalScopes = (
+  terminalManager: Pick<TerminalManagerShape, "close" | "closeSessionsOpenedAtOrBefore">,
+  threadId: ThreadId,
+  deleteHistory: boolean,
+  openedAtOrBefore?: string,
+) =>
+  Effect.forEach(terminalScopeIdsForThread(threadId), (scopeId) =>
+    cleanupSucceededUnlessInterrupted({
+      effect:
+        openedAtOrBefore === undefined
+          ? terminalManager.close({ threadId: ThreadId.makeUnsafe(scopeId), deleteHistory })
+          : terminalManager.closeSessionsOpenedAtOrBefore({
+              threadId: ThreadId.makeUnsafe(scopeId),
+              openedAtOrBefore,
+            }),
+      message: "thread lifecycle cleanup skipped terminal close",
+      threadId: ThreadId.makeUnsafe(scopeId),
+    }),
+  ).pipe(Effect.map((results) => results.every(Boolean)));
 
 const make = Effect.gen(function* () {
   const orchestrationEngine = yield* OrchestrationEngineService;
@@ -157,18 +178,7 @@ const make = Effect.gen(function* () {
     threadId: ThreadDeletedEvent["payload"]["threadId"],
     deleteHistory: boolean,
     openedAtOrBefore?: string,
-  ) =>
-    cleanupSucceededUnlessInterrupted({
-      effect:
-        openedAtOrBefore === undefined
-          ? terminalManager.close({ threadId, deleteHistory })
-          : terminalManager.closeSessionsOpenedAtOrBefore({
-              threadId,
-              openedAtOrBefore,
-            }),
-      message: "thread lifecycle cleanup skipped terminal close",
-      threadId,
-    });
+  ) => closeThreadTerminalScopes(terminalManager, threadId, deleteHistory, openedAtOrBefore);
 
   const waitForThreadPurgeFence = Effect.fn(function* (
     threadId: ThreadDeletedEvent["payload"]["threadId"],

redxzeta/forkara

apps/server/src/orchestration/Layers/StreamingPersistence.benchmark.test.ts
@@ -1,0 +1,198 @@
+import { mkdtemp, readFile, rm, stat, writeFile } from "node:fs/promises";
+import { createHash } from "node:crypto";
+import { tmpdir, platform, release, totalmem, cpus } from "node:os";
+import { join } from "node:path";
+import { monitorEventLoopDelay } from "node:perf_hooks";
+import { setImmediate } from "node:timers/promises";
+import * as NodeServices from "@effect/platform-node/NodeServices";
+import {
+  CommandId,
+  DEFAULT_PROVIDER_INTERACTION_MODE,
+  MessageId,
+  ProjectId,
+  ThreadId,
+} from "@forkara/contracts";
+import { Effect, Layer, ManagedRuntime } from "effect";
+import * as SqlClient from "effect/unstable/sql/SqlClient";
+import { expect, it } from "vitest";
+
+import { ServerConfig } from "../../config.ts";
+import { makeSqlitePersistenceLive } from "../../persistence/Layers/Sqlite.ts";
+import { OrchestrationEventStoreLive } from "../../persistence/Layers/OrchestrationEventStore.ts";
+import { OrchestrationCommandReceiptRepositoryLive } from "../../persistence/Layers/OrchestrationCommandReceipts.ts";
+import { ProjectionSnapshotQuery } from "../Services/ProjectionSnapshotQuery.ts";
+import { OrchestrationEngineService } from "../Services/OrchestrationEngine.ts";
+import { OrchestrationEngineLive } from "./OrchestrationEngine.ts";
+import { OrchestrationProjectionPipelineLive } from "./ProjectionPipeline.ts";
+import { OrchestrationProjectionSnapshotQueryLive } from "./ProjectionSnapshotQuery.ts";
+
+// Opt-in only. Each invocation uses a fresh process and a disposable database.
+it.skipIf(!process.env.SYNARA_STREAMING_BENCHMARK_OUTPUT)(
+  "measures the production streaming engine",
+  async () => {
+    const output = process.env.SYNARA_STREAMING_BENCHMARK_OUTPUT!;
+    const messageBytes = Number(process.env.SYNARA_STREAMING_BENCHMARK_BYTES ?? 200_000);
+    const chunkBytes = 40;
+    const threadCount = Number(process.env.SYNARA_STREAMING_BENCHMARK_THREADS ?? 1);
+    const dir = await mkdtemp(join(tmpdir(), "forkara-streaming-benchmark-"));
+    const dbPath = join(dir, "state.sqlite");
+    const runtime = ManagedRuntime.make(
+      OrchestrationEngineLive.pipe(
+        Layer.provide(OrchestrationProjectionPipelineLive),
+        Layer.provideMerge(OrchestrationProjectionSnapshotQueryLive),
+        Layer.provide(OrchestrationEventStoreLive),
+        Layer.provide(OrchestrationCommandReceiptRepositoryLive),
+        Layer.provideMerge(makeSqlitePersistenceLive(dbPath)),
+        Layer.provideMerge(ServerConfig.layerTest(process.cwd(), dir)),
+        Layer.provideMerge(NodeServices.layer),
+      ),
+    );
+    const delay = monitorEventLoopDelay({ resolution: 10 });
+    try {
+      const engine = await runtime.runPromise(Effect.service(OrchestrationEngineService));
+      const snapshots = await runtime.runPromise(Effect.service(ProjectionSnapshotQuery));
+      const sql = await runtime.runPromise(Effect.service(SqlClient.SqlClient));
+      const createdAt = "2026-09-10T12:00:00.000Z";
+      const projectId = ProjectId.makeUnsafe("benchmark-project");
+      await runtime.runPromise(
+        engine.dispatch({
+          type: "project.create",
+          commandId: CommandId.makeUnsafe("project"),
+          projectId,
+          title: "Benchmark",
+          workspaceRoot: dir,
+          defaultModelSelection: null,
+          createdAt,
+        }),
+      );
+      const threads = Array.from({ length: threadCount }, (_, index) =>
+        ThreadId.makeUnsafe(`benchmark-thread-${index}`),
+      );
+      for (const threadId of threads) {
+        await runtime.runPromise(
+          engine.dispatch({
+            type: "thread.create",
+            commandId: CommandId.makeUnsafe(threadId),
+            threadId,
+            projectId,
+            title: "Benchmark",
+            modelSelection: { provider: "codex", model: "gpt-5-codex" },
+            interactionMode: DEFAULT_PROVIDER_INTERACTION_MODE,
+            runtimeMode: "approval-required",
+            branch: null,
+            worktreePath: null,
+            createdAt,
+          }),
+        );
+      }
+      await runtime.runPromise(sql`PRAGMA wal_autocheckpoint = 0`);
+      await runtime.runPromise(sql`PRAGMA wal_checkpoint(TRUNCATE)`);
+      const memoryBefore = process.memoryUsage();
+      let peakRss = memoryBefore.rss;
+      let peakHeapUsed = memoryBefore.heapUsed;
+      delay.enable();
+      await setImmediate();
+      const started = performance.now();
+      for (let offset = 0; offset < messageBytes; offset += chunkBytes) {
+        for (const threadId of threads) {
+          await runtime.runPromise(
+            engine.dispatch({
+              type: "thread.message.assistant.delta",
+              commandId: CommandId.makeUnsafe(`${threadId}-${offset}`),
+              threadId,
+              messageId: MessageId.makeUnsafe("benchmark-message"),
+              delta: "x".repeat(Math.min(chunkBytes, messageBytes - offset)),
+              createdAt,
+            }),
+          );
+        }
+        if ((offset / chunkBytes) % 100 === 0) {
+          const memory = process.memoryUsage();
+          peakRss = Math.max(peakRss, memory.rss);
+          peakHeapUsed = Math.max(peakHeapUsed, memory.heapUsed);
+          // Yield every 100 chunks so timer delay has a reproducible sampling window.
+          await setImmediate();
+        }
+      }
+      await setImmediate();
+      const streamMs = performance.now() - started;
+      delay.disable();
+      const streamingWalBytes = (await stat(`${dbPath}-wal`)).size;
+      const readStarted = performance.now();
+      const streaming = await runtime.runPromise(snapshots.getSnapshot());
+      const streamingReadMs = performance.now() - readStarted;
+      expect(streaming.threads).toHaveLength(threadCount);
+      for (const thread of streaming.threads)
+        expect(thread.messages[0]?.text).toBe("x".repeat(messageBytes));
+      const completionStarted = performance.now();
+      for (const threadId of threads) {
+        await runtime.runPromise(
+          engine.dispatch({
+            type: "thread.message.assistant.complete",
+            commandId: CommandId.makeUnsafe(`${threadId}-complete`),
+            threadId,
+            messageId: MessageId.makeUnsafe("benchmark-message"),
+            createdAt,
+          }),
+        );
+      }
+      const completionMs = performance.now() - completionStarted;
+      const completed = await runtime.runPromise(snapshots.getSnapshot());
+      expect(completed.threads).toHaveLength(threadCount);
+      for (const thread of completed.threads)
+        expect(thread.messages[0]?.text).toBe("x".repeat(messageBytes));
+      const finalWalBytes = (await stat(`${dbPath}-wal`)).size;
+      const hashes: Record<string, string> = {};
+      for (const path of [
+        "src/orchestration/Layers/ProjectionPipeline.ts",
+        "src/persistence/Layers/ProjectionThreadMessages.ts",
+        "src/persistence/Migrations.ts",
+        "src/orchestration/Layers/OrchestrationEngine.ts",
+        "src/persistence/messageTextChunks.ts",
+      ])
+        hashes[path] = createHash("sha256")
+          .update(await readFile(path))
+          .digest("hex");
+      await writeFile(
+        output,
+        JSON.stringify(
+          {
+            environment: {
+              node: process.version,
+              os: `${platform()} ${release()}`,
+              cpu: cpus()[0]?.model,
+              totalMemoryBytes: totalmem(),
+              sqlite: await runtime.runPromise(sql`SELECT sqlite_version() AS version`),
+            },
+            workload: {
+              messageBytes,
+              chunkBytes,
+              threadCount,
+              transactions: Math.ceil(messageBytes / chunkBytes) * threadCount,
+              checkpoint: "disabled during measurement; setup WAL truncated",
+              timerYieldEveryChunks: 100,
+            },
+            streamingWalBytes,
+            finalWalBytes,
+            streamMs,
+            streamingReadMs,
+            completionMs,
+            eventLoopDelayMs: { max: delay.max / 1e6, p99: delay.percentile(99) / 1e6 },
+            memoryBefore,
+            memoryAfter: process.memoryUsage(),
+            sampledPeakRss: peakRss,
+            sampledPeakHeapUsed: peakHeapUsed,
+            hashes,
+          },
+          null,
+          2,
+        ),
+      );
+    } finally {
+      delay.disable();
+      await runtime.dispose();
+      await rm(dir, { recursive: true, force: true });
+    }
+  },
+  180_000,
+);

apps/server/src/orchestration/Layers/ThreadDeletionReactor.ts
@@ -3,6 +3,7 @@ import {
   makeDrainableWorker,
   startDrainableWorkerProducers,
 } from "@forkara/shared/DrainableWorker";
+import { terminalScopeIdsForThread } from "@forkara/shared/terminalThreads";
 import { Cause, Effect, Layer, Option, Stream } from "effect";
 
 import { ServerConfig } from "../../config";
@@ -11,7 +12,7 @@ import { GitCore } from "../../git/Services/GitCore";
 import { pruneProjectedArchivedManagedWorktrees } from "../../managedWorktrees";
 import { ProfileStatsArchive } from "../../profileStatsArchive";
 import { ProviderService } from "../../provider/Services/ProviderService";
-import { TerminalManager } from "../../terminal/Services/Manager";
+import { TerminalManager, type TerminalManagerShape } from "../../terminal/Services/Manager";
 import { THREAD_RETENTION_COMMAND_ID_PREFIX } from "../../threadRetention";
 import { OrchestrationEngineService } from "../Services/OrchestrationEngine";
 import { ProjectionSnapshotQuery } from "../Services/ProjectionSnapshotQuery";
@@ -106,6 +107,26 @@ export const detachThreadDevice = (threadId: ThreadId) =>
     ),
   );
 
+export const closeThreadTerminalScopes = (
+  terminalManager: Pick<TerminalManagerShape, "close" | "closeSessionsOpenedAtOrBefore">,
+  threadId: ThreadId,
+  deleteHistory: boolean,
+  openedAtOrBefore?: string,
+) =>
+  Effect.forEach(terminalScopeIdsForThread(threadId), (scopeId) =>
+    cleanupSucceededUnlessInterrupted({
+      effect:
+        openedAtOrBefore === undefined
+          ? terminalManager.close({ threadId: ThreadId.makeUnsafe(scopeId), deleteHistory })
+          : terminalManager.closeSessionsOpenedAtOrBefore({
+              threadId: ThreadId.makeUnsafe(scopeId),
+              openedAtOrBefore,
+            }),
+      message: "thread lifecycle cleanup skipped terminal close",
+      threadId: ThreadId.makeUnsafe(scopeId),
+    }),
+  ).pipe(Effect.map((results) => results.every(Boolean)));
+
 const make = Effect.gen(function* () {
   const orchestrationEngine = yield* OrchestrationEngineService;
   const profileStatsArchive = yield* ProfileStatsArchive;
@@ -181,18 +202,7 @@ const make = Effect.gen(function* () {
     threadId: ThreadDeletedEvent["payload"]["threadId"],
     deleteHistory: boolean,
     openedAtOrBefore?: string,
-  ) =>
-    cleanupSucceededUnlessInterrupted({
-      effect:
-        openedAtOrBefore === undefined
-          ? terminalManager.close({ threadId, deleteHistory })
-          : terminalManager.closeSessionsOpenedAtOrBefore({
-              threadId,
-              openedAtOrBefore,
-            }),
-      message: "thread lifecycle cleanup skipped terminal close",
-      threadId,
-    });
+  ) => closeThreadTerminalScopes(terminalManager, threadId, deleteHistory, openedAtOrBefore);
 
   const waitForThreadPurgeFence = Effect.fn(function* (
     threadId: ThreadDeletedEvent["payload"]["threadId"],