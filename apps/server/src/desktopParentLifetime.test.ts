Neonity2020/synara

Fix desktop backend lifetime after parent exit (#1154)
Tie the desktop backend lifetime to its owning Electron process through stdin EOF. Run existing cleanup on parent loss and terminate only the backend if cleanup hangs.

Preserve CLI stdin behavior and database ownership protections. All 22 PR checks passed and Cursor Bugbot reported zero findings.

apps/server/src/desktopParentLifetime.test.ts
@@ -1,0 +1,118 @@
+import { once } from "node:events";
+import { PassThrough } from "node:stream";
+
+import { Deferred, Effect, Fiber } from "effect";
+import { describe, expect, it, vi } from "vitest";
+
+import { consumeDesktopParentInput, withDesktopParentLifetime } from "./desktopParentLifetime";
+
+function runningProgram(onStarted: () => void, onReleased: () => void) {
+  return Effect.acquireUseRelease(
+    Effect.sync(onStarted),
+    () => Effect.never,
+    () => Effect.sync(onReleased),
+  );
+}
+
+describe("desktop parent lifetime", () => {
+  it("consumes the marker once and leaves ordinary CLI input untouched", () => {
+    const input = new PassThrough();
+    const readInput = vi.fn(() => input);
+    const env: NodeJS.ProcessEnv = { SYNARA_DESKTOP_PARENT_STDIN: "1" };
+    expect(consumeDesktopParentInput(env, readInput)).toBe(input);
+    expect(env.SYNARA_DESKTOP_PARENT_STDIN).toBeUndefined();
+    expect(consumeDesktopParentInput(env, readInput)).toBeUndefined();
+    expect(readInput).toHaveBeenCalledTimes(1);
+    expect(
+      consumeDesktopParentInput({ SYNARA_DESKTOP_PARENT_STDIN: "0" }, readInput),
+    ).toBeUndefined();
+    expect(readInput).toHaveBeenCalledTimes(1);
+  });
+
+  it.each(["end", "close", "error"] as const)(
+    "cleans up the runtime on input %s",
+    async (event) => {
+      const input = new PassThrough();
+      const released = vi.fn();
+      const started = Promise.withResolvers<void>();
+      const result = Effect.runPromise(
+        withDesktopParentLifetime(runningProgram(started.resolve, released), input),
+      );
+      await started.promise;
+      expect(released).not.toHaveBeenCalled();
+      if (event === "end") input.end();
+      else if (event === "close") input.destroy();
+      else input.destroy(new Error("owner pipe failed"));
+      await result;
+      expect(released).toHaveBeenCalledTimes(1);
+      for (const name of ["end", "close", "error"]) expect(input.listenerCount(name)).toBe(0);
+    },
+  );
+
+  it("does not start when the owner pipe is already closed", async () => {
+    const input = new PassThrough();
+    input.destroy();
+    const started = vi.fn();
+    await Effect.runPromise(withDesktopParentLifetime(Effect.sync(started), input));
+    expect(started).not.toHaveBeenCalled();
+  });
+
+  it("does not start when EOF was delivered before the watcher attached", async () => {
+    const input = new PassThrough({ autoDestroy: false });
+    const ended = once(input, "end");
+    input.resume();
+    input.end();
+    await ended;
+    expect(input.destroyed).toBe(false);
+    const started = vi.fn();
+    await Effect.runPromise(withDesktopParentLifetime(Effect.sync(started), input));
+    expect(started).not.toHaveBeenCalled();
+  });
+
+  it("preserves signal-driven interruption while the owner remains alive", async () => {
+    const input = new PassThrough();
+    const started = Promise.withResolvers<void>();
+    const released = vi.fn();
+    const fiber = Effect.runFork(
+      withDesktopParentLifetime(runningProgram(started.resolve, released), input),
+    );
+    await started.promise;
+    await Effect.runPromise(Fiber.interrupt(fiber));
+    expect(released).toHaveBeenCalledOnce();
+    for (const name of ["end", "close", "error"]) expect(input.listenerCount(name)).toBe(0);
+  });
+
+  it("waits for resource finalizers before finishing parent-loss shutdown", async () => {
+    const input = new PassThrough();
+    const gate = Deferred.makeUnsafe<void>();
+    const finalizing = Promise.withResolvers<void>();
+    const started = Promise.withResolvers<void>();
+    const completed = vi.fn();
+    const program = Effect.acquireUseRelease(
+      Effect.sync(started.resolve),
+      () => Effect.never,
+      () => Effect.sync(finalizing.resolve).pipe(Effect.andThen(Deferred.await(gate))),
+    );
+    const result = Effect.runPromise(withDesktopParentLifetime(program, input)).then(completed);
+    await started.promise;
+    input.end();
+    await finalizing.promise;
+    expect(completed).not.toHaveBeenCalled();
+    Deferred.doneUnsafe(gate, Effect.void);
+    await result;
+    expect(completed).toHaveBeenCalledOnce();
+  });
+
+  it("preserves normal completion and failure while the owner stays alive", async () => {
+    const input = new PassThrough();
+    expect(await Effect.runPromise(withDesktopParentLifetime(Effect.succeed(42), input))).toBe(42);
+    await expect(
+      Effect.runPromise(withDesktopParentLifetime(Effect.fail("startup failed"), input)),
+    ).rejects.toThrow("startup failed");
+    expect(input.isPaused()).toBe(true);
+    for (const name of ["end", "close", "error"]) expect(input.listenerCount(name)).toBe(0);
+    expect(await Effect.runPromise(withDesktopParentLifetime(Effect.succeed(42), undefined))).toBe(
+      42,
+    );
+  });
+});

apps/server/src/desktopParentLifetime.ts
@@ -1,0 +1,67 @@
+import type { Readable } from "node:stream";
+
+import { Deferred, Effect } from "effect";
+
+/** Consume the desktop-only marker before provider children can inherit it. */
+export function consumeDesktopParentInput(
+  env: NodeJS.ProcessEnv,
+  input: () => Readable,
+): Readable | undefined {
+  const enabled = env.SYNARA_DESKTOP_PARENT_STDIN === "1";
+  delete env.SYNARA_DESKTOP_PARENT_STDIN;
+  return enabled ? input() : undefined;
+}
+
+/** The desktop holds stdin open for exactly its own lifetime, including crashes. */
+export function withDesktopParentLifetime<A, E, R>(
+  program: Effect.Effect<A, E, R>,
+  input: Readable | undefined,
+  shutdownTimeoutMs = 20_000,
+): Effect.Effect<A | void, E, R> {
+  if (!input) return program;
+
+  return Effect.gen(function* () {
+    const disconnected = yield* Deferred.make<void>();
+    let shutdownTimer: ReturnType<typeof setTimeout> | undefined;
+    let ownerLost = false;
+    yield* Effect.acquireRelease(
+      Effect.sync(() => {
+        const onDisconnect = () => {
+          if (ownerLost) return;
+          ownerLost = true;
+          Deferred.doneUnsafe(disconnected, Effect.void);
+          // The desktop's usual shutdown watchdog is gone. Bound hung runtime
+          // finalizers without touching another process or bypassing its lock.
+          shutdownTimer = setTimeout(() => {
+            process.stderr.write("Desktop owner lost: backend shutdown timed out.\n");
+            process.exit(1);
+          }, shutdownTimeoutMs);
+          shutdownTimer.unref();
+        };
+        input.on("end", onDisconnect);
+        input.on("close", onDisconnect);
+        input.on("error", onDisconnect);
+        input.resume();
+        if (input.readableEnded || input.destroyed) onDisconnect();
+        return onDisconnect;
+      }),
+      (onDisconnect) =>
+        Effect.sync(() => {
+          if (shutdownTimer) clearTimeout(shutdownTimer);
+          input.off("end", onDisconnect);
+          input.off("close", onDisconnect);
+          input.off("error", onDisconnect);
+          input.pause();
+        }),
+    );
+
+    // Install the watcher before startup acquires the database or any children.
+    if (ownerLost) return;
+    return yield* Effect.raceFirst(
+      program,
+      Deferred.await(disconnected).pipe(
+        Effect.andThen(Effect.logInfo("Desktop owner connection closed; shutting down backend")),
+      ),
+    );
+  }).pipe(Effect.scoped);
+}

apps/server/src/fixtures/desktopParentLifetime.ts
@@ -1,0 +1,25 @@
+import * as fs from "node:fs";
+
+import { Effect } from "effect";
+
+import { consumeDesktopParentInput, withDesktopParentLifetime } from "../desktopParentLifetime.ts";
+import { withDatabaseLifecycleLock } from "../persistence/DatabaseLifecycleLock.ts";
+
+const [dbPath, statePath, mode] = process.argv.slice(2) as [string, string, string];
+const input = consumeDesktopParentInput(process.env, () => process.stdin);
+const keepAlive = setInterval(() => undefined, 1_000);
+const program = withDatabaseLifecycleLock(
+  dbPath,
+  Effect.sync(() => {
+    fs.writeFileSync(
+      statePath,
+      JSON.stringify({ pid: process.pid, marker: process.env.SYNARA_DESKTOP_PARENT_STDIN ?? null }),
+    );
+  }).pipe(
+    Effect.andThen(Effect.never),
+    Effect.ensuring(mode === "stubborn" ? Effect.never : Effect.void),
+  ),
+);
+await Effect.runPromise(withDesktopParentLifetime(program, input, 500));
+clearInterval(keepAlive);
+fs.writeFileSync(`${statePath}.stopped`, "cleaned up");

apps/server/src/fixtures/desktopParentOwner.mjs
@@ -1,0 +1,12 @@
+import { spawn } from "node:child_process";
+
+const [fixture, dbPath, statePath, mode] = process.argv.slice(2);
+const backend = spawn(process.execPath, [fixture, dbPath, statePath, mode], {
+  env: { ...process.env, SYNARA_DESKTOP_PARENT_STDIN: "1" },
+  stdio: ["pipe", "ignore", "pipe"],
+});
+backend.stderr.pipe(process.stderr);
+backend.on("exit", (code, signal) => process.send?.({ code, signal }));
+process.on("message", (message) => {
+  if (message === "close") backend.stdin.end();
+});

redxzeta/forkara

apps/server/src/desktopParentLifetime.test.ts
@@ -1,0 +1,118 @@
+import { once } from "node:events";
+import { PassThrough } from "node:stream";
+
+import { Deferred, Effect, Fiber } from "effect";
+import { describe, expect, it, vi } from "vitest";
+
+import { consumeDesktopParentInput, withDesktopParentLifetime } from "./desktopParentLifetime";
+
+function runningProgram(onStarted: () => void, onReleased: () => void) {
+  return Effect.acquireUseRelease(
+    Effect.sync(onStarted),
+    () => Effect.never,
+    () => Effect.sync(onReleased),
+  );
+}
+
+describe("desktop parent lifetime", () => {
+  it("consumes the marker once and leaves ordinary CLI input untouched", () => {
+    const input = new PassThrough();
+    const readInput = vi.fn(() => input);
+    const env: NodeJS.ProcessEnv = { FORKARA_DESKTOP_PARENT_STDIN: "1" };
+    expect(consumeDesktopParentInput(env, readInput)).toBe(input);
+    expect(env.FORKARA_DESKTOP_PARENT_STDIN).toBeUndefined();
+    expect(consumeDesktopParentInput(env, readInput)).toBeUndefined();
+    expect(readInput).toHaveBeenCalledTimes(1);
+    expect(
+      consumeDesktopParentInput({ FORKARA_DESKTOP_PARENT_STDIN: "0" }, readInput),
+    ).toBeUndefined();
+    expect(readInput).toHaveBeenCalledTimes(1);
+  });
+
+  it.each(["end", "close", "error"] as const)(
+    "cleans up the runtime on input %s",
+    async (event) => {
+      const input = new PassThrough();
+      const released = vi.fn();
+      const started = Promise.withResolvers<void>();
+      const result = Effect.runPromise(
+        withDesktopParentLifetime(runningProgram(started.resolve, released), input),
+      );
+      await started.promise;
+      expect(released).not.toHaveBeenCalled();
+      if (event === "end") input.end();
+      else if (event === "close") input.destroy();
+      else input.destroy(new Error("owner pipe failed"));
+      await result;
+      expect(released).toHaveBeenCalledTimes(1);
+      for (const name of ["end", "close", "error"]) expect(input.listenerCount(name)).toBe(0);
+    },
+  );
+
+  it("does not start when the owner pipe is already closed", async () => {
+    const input = new PassThrough();
+    input.destroy();
+    const started = vi.fn();
+    await Effect.runPromise(withDesktopParentLifetime(Effect.sync(started), input));
+    expect(started).not.toHaveBeenCalled();
+  });
+
+  it("does not start when EOF was delivered before the watcher attached", async () => {
+    const input = new PassThrough({ autoDestroy: false });
+    const ended = once(input, "end");
+    input.resume();
+    input.end();
+    await ended;
+    expect(input.destroyed).toBe(false);
+    const started = vi.fn();
+    await Effect.runPromise(withDesktopParentLifetime(Effect.sync(started), input));
+    expect(started).not.toHaveBeenCalled();
+  });
+
+  it("preserves signal-driven interruption while the owner remains alive", async () => {
+    const input = new PassThrough();
+    const started = Promise.withResolvers<void>();
+    const released = vi.fn();
+    const fiber = Effect.runFork(
+      withDesktopParentLifetime(runningProgram(started.resolve, released), input),
+    );
+    await started.promise;
+    await Effect.runPromise(Fiber.interrupt(fiber));
+    expect(released).toHaveBeenCalledOnce();
+    for (const name of ["end", "close", "error"]) expect(input.listenerCount(name)).toBe(0);
+  });
+
+  it("waits for resource finalizers before finishing parent-loss shutdown", async () => {
+    const input = new PassThrough();
+    const gate = Deferred.makeUnsafe<void>();
+    const finalizing = Promise.withResolvers<void>();
+    const started = Promise.withResolvers<void>();
+    const completed = vi.fn();
+    const program = Effect.acquireUseRelease(
+      Effect.sync(started.resolve),
+      () => Effect.never,
+      () => Effect.sync(finalizing.resolve).pipe(Effect.andThen(Deferred.await(gate))),
+    );
+    const result = Effect.runPromise(withDesktopParentLifetime(program, input)).then(completed);
+    await started.promise;
+    input.end();
+    await finalizing.promise;
+    expect(completed).not.toHaveBeenCalled();
+    Deferred.doneUnsafe(gate, Effect.void);
+    await result;
+    expect(completed).toHaveBeenCalledOnce();
+  });
+
+  it("preserves normal completion and failure while the owner stays alive", async () => {
+    const input = new PassThrough();
+    expect(await Effect.runPromise(withDesktopParentLifetime(Effect.succeed(42), input))).toBe(42);
+    await expect(
+      Effect.runPromise(withDesktopParentLifetime(Effect.fail("startup failed"), input)),
+    ).rejects.toThrow("startup failed");
+    expect(input.isPaused()).toBe(true);
+    for (const name of ["end", "close", "error"]) expect(input.listenerCount(name)).toBe(0);
+    expect(await Effect.runPromise(withDesktopParentLifetime(Effect.succeed(42), undefined))).toBe(
+      42,
+    );
+  });
+});

apps/server/src/desktopParentLifetime.ts
@@ -1,0 +1,67 @@
+import type { Readable } from "node:stream";
+
+import { Deferred, Effect } from "effect";
+
+/** Consume the desktop-only marker before provider children can inherit it. */
+export function consumeDesktopParentInput(
+  env: NodeJS.ProcessEnv,
+  input: () => Readable,
+): Readable | undefined {
+  const enabled = env.FORKARA_DESKTOP_PARENT_STDIN === "1";
+  delete env.FORKARA_DESKTOP_PARENT_STDIN;
+  return enabled ? input() : undefined;
+}
+
+/** The desktop holds stdin open for exactly its own lifetime, including crashes. */
+export function withDesktopParentLifetime<A, E, R>(
+  program: Effect.Effect<A, E, R>,
+  input: Readable | undefined,
+  shutdownTimeoutMs = 20_000,
+): Effect.Effect<A | void, E, R> {
+  if (!input) return program;
+
+  return Effect.gen(function* () {
+    const disconnected = yield* Deferred.make<void>();
+    let shutdownTimer: ReturnType<typeof setTimeout> | undefined;
+    let ownerLost = false;
+    yield* Effect.acquireRelease(
+      Effect.sync(() => {
+        const onDisconnect = () => {
+          if (ownerLost) return;
+          ownerLost = true;
+          Deferred.doneUnsafe(disconnected, Effect.void);
+          // The desktop's usual shutdown watchdog is gone. Bound hung runtime
+          // finalizers without touching another process or bypassing its lock.
+          shutdownTimer = setTimeout(() => {
+            process.stderr.write("Desktop owner lost: backend shutdown timed out.\n");
+            process.exit(1);
+          }, shutdownTimeoutMs);
+          shutdownTimer.unref();
+        };
+        input.on("end", onDisconnect);
+        input.on("close", onDisconnect);
+        input.on("error", onDisconnect);
+        input.resume();
+        if (input.readableEnded || input.destroyed) onDisconnect();
+        return onDisconnect;
+      }),
+      (onDisconnect) =>
+        Effect.sync(() => {
+          if (shutdownTimer) clearTimeout(shutdownTimer);
+          input.off("end", onDisconnect);
+          input.off("close", onDisconnect);
+          input.off("error", onDisconnect);
+          input.pause();
+        }),
+    );
+
+    // Install the watcher before startup acquires the database or any children.
+    if (ownerLost) return;
+    return yield* Effect.raceFirst(
+      program,
+      Deferred.await(disconnected).pipe(
+        Effect.andThen(Effect.logInfo("Desktop owner connection closed; shutting down backend")),
+      ),
+    );
+  }).pipe(Effect.scoped);
+}

apps/server/src/fixtures/desktopParentLifetime.ts
@@ -1,0 +1,25 @@
+import * as fs from "node:fs";
+
+import { Effect } from "effect";
+
+import { consumeDesktopParentInput, withDesktopParentLifetime } from "../desktopParentLifetime.ts";
+import { withDatabaseLifecycleLock } from "../persistence/DatabaseLifecycleLock.ts";
+
+const [dbPath, statePath, mode] = process.argv.slice(2) as [string, string, string];
+const input = consumeDesktopParentInput(process.env, () => process.stdin);
+const keepAlive = setInterval(() => undefined, 1_000);
+const program = withDatabaseLifecycleLock(
+  dbPath,
+  Effect.sync(() => {
+    fs.writeFileSync(
+      statePath,
+      JSON.stringify({ pid: process.pid, marker: process.env.FORKARA_DESKTOP_PARENT_STDIN ?? null }),
+    );
+  }).pipe(
+    Effect.andThen(Effect.never),
+    Effect.ensuring(mode === "stubborn" ? Effect.never : Effect.void),
+  ),
+);
+await Effect.runPromise(withDesktopParentLifetime(program, input, 500));
+clearInterval(keepAlive);
+fs.writeFileSync(`${statePath}.stopped`, "cleaned up");

apps/server/src/fixtures/desktopParentOwner.mjs
@@ -1,0 +1,12 @@
+import { spawn } from "node:child_process";
+
+const [fixture, dbPath, statePath, mode] = process.argv.slice(2);
+const backend = spawn(process.execPath, [fixture, dbPath, statePath, mode], {
+  env: { ...process.env, FORKARA_DESKTOP_PARENT_STDIN: "1" },
+  stdio: ["pipe", "ignore", "pipe"],
+});
+backend.stderr.pipe(process.stderr);
+backend.on("exit", (code, signal) => process.send?.({ code, signal }));
+process.on("message", (message) => {
+  if (message === "close") backend.stdin.end();
+});