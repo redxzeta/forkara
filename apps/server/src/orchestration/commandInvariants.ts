Emanuele-web04/synara

fix(approvals): reconcile duplicate response races (#1102)
* fix(approvals): reconcile duplicate response races

* style: format approval response fix

* fix(approvals): satisfy type and compiler guards

* Preserve undo guards and invalidate orphaned interaction callbacks

---------

Co-authored-by: iiiMohammed <311510718+iiiMohammed@users.noreply.github.com>
Co-authored-by: Emanuele Di Pietro <127427797+Emanuele-web04@users.noreply.github.com>

apps/server/src/orchestration/commandInvariants.ts
@@ -14,7 +14,10 @@ import type {
   ThreadId,
   TurnId,
 } from "@synara/contracts";
-import { THREAD_NOT_ARCHIVED_INVARIANT_MARKER } from "@synara/shared/errorMessages";
+import {
+  APPROVAL_ALREADY_ANSWERED_INVARIANT_MARKER,
+  THREAD_NOT_ARCHIVED_INVARIANT_MARKER,
+} from "@synara/shared/errorMessages";
 import {
   isLegacyHomeChatContainerRow as isSharedLegacyHomeChatContainerRow,
   isOrdinaryProjectRow as isSharedOrdinaryProjectRow,
@@ -402,7 +405,7 @@ export function requireApprovalNotResponded(input: {
   return Effect.fail(
     invariantError(
       input.command.type,
-      `Approval request '${input.requestId}' on thread '${input.threadId}' was already answered.`,
+      `Approval request '${input.requestId}' on thread '${input.threadId}' ${APPROVAL_ALREADY_ANSWERED_INVARIANT_MARKER}`,
     ),
   );
 }

apps/server/src/orchestration/startupTurnReconciliation.ts
@@ -30,12 +30,14 @@
  */
 import type {
   OrchestrationCommand,
+  OrchestrationPendingInteraction,
   OrchestrationThreadActivity,
   OrchestrationSession,
   RuntimeMode,
   ThreadId,
 } from "@synara/contracts";
 import { CommandId, EventId } from "@synara/contracts";
+import { createStalePendingInteractionMatcher } from "@synara/shared/pendingInteractions";
 import {
   buildStalePendingRequestFailureDetail,
   derivePendingThreadRequestIds,
@@ -71,6 +73,14 @@ export interface ReconcilableThread {
   readonly activities?: ReadonlyArray<
     Pick<OrchestrationThreadActivity, "createdAt" | "id" | "kind" | "payload" | "sequence">
   >;
+  readonly pendingInteractions?:
+    | ReadonlyArray<
+        Pick<
+          OrchestrationPendingInteraction,
+          "interactionKind" | "requestId" | "lifecycleGeneration" | "status" | "createdAt"
+        >
+      >
+    | undefined;
 }
 
 /**
@@ -96,10 +106,39 @@ function planStalePendingRequestCommands(input: {
   readonly thread: ReconcilableThread;
   readonly now: string;
 }): ReadonlyArray<ThreadActivityAppendCommand> {
+  const commands: ThreadActivityAppendCommand[] = [];
+  if (input.thread.pendingInteractions !== undefined) {
+    const isAlreadyStale = createStalePendingInteractionMatcher(input.thread.activities ?? []);
+    for (const interaction of input.thread.pendingInteractions) {
+      // A process restart loses every live provider callback. Pending,
+      // responding, and previously retryable rows are therefore no longer
+      // answerable. Uncertain user-input responses are also retryable unless
+      // their callback has already been explicitly invalidated.
+      if (
+        interaction.status === "confirmed" ||
+        (interaction.status === "uncertain" &&
+          (interaction.interactionKind === "approval" || isAlreadyStale(interaction)))
+      ) {
+        continue;
+      }
+      commands.push(
+        buildStalePendingRequestCommand({
+          threadId: input.thread.id,
+          now: input.now,
+          requestKind: interaction.interactionKind === "approval" ? "approval" : "user-input",
+          requestId: interaction.requestId,
+          ...(interaction.lifecycleGeneration !== null
+            ? { lifecycleGeneration: interaction.lifecycleGeneration }
+            : {}),
+        }),
+      );
+    }
+    return commands;
+  }
+
   const pendingRequestIds = derivePendingThreadRequestIds({
     activities: input.thread.activities ?? [],
   });
-  const commands: ThreadActivityAppendCommand[] = [];
   for (const requestId of pendingRequestIds.approvalRequestIds) {
     commands.push(
       buildStalePendingRequestCommand({
@@ -155,6 +194,7 @@ function buildStalePendingRequestCommand(input: {
   readonly now: string;
   readonly requestKind: PendingThreadRequestKind;
   readonly requestId: string;
+  readonly lifecycleGeneration?: string;
 }): ThreadActivityAppendCommand {
   const commandKey = [
     "restart-reconcile",
@@ -178,6 +218,9 @@ function buildStalePendingRequestCommand(input: {
       payload: {
         detail: buildStalePendingRequestFailureDetail(input.requestKind, input.requestId),
         requestId: input.requestId,
+        ...(input.lifecycleGeneration !== undefined
+          ? { lifecycleGeneration: input.lifecycleGeneration }
+          : {}),
       },
       turnId: null,
       createdAt: input.now,

apps/web/src/components/ChatView.tsx
@@ -41,6 +41,11 @@ import {
   RuntimeMode,
 } from "@synara/contracts";
 import { automationRequiresTargetThread } from "@synara/shared/automationMode";
+import {
+  APPROVAL_ALREADY_ANSWERED_INVARIANT_MARKER,
+  collectErrorMessages,
+  describeErrorMessage,
+} from "@synara/shared/errorMessages";
 import { respondingInteractionReclaimAt } from "@synara/shared/pendingInteractions";
 import { providerSupportsNativeTurnSteering } from "@synara/shared/providerMetadata";
 import { getDefaultModel, getModelCapabilities, normalizeModelSlug } from "@synara/shared/model";
@@ -249,7 +254,10 @@ import {
   createSidechatSummariesForSourceSelector,
   createThreadSelector,
 } from "../storeSelectors";
-import { buildThreadSubscribeInput } from "../threadDetailResumeCursors";
+import {
+  buildThreadSubscribeInput,
+  clearThreadDetailResumeCursor,
+} from "../threadDetailResumeCursors";
 import { retainThreadDetailSubscription } from "../threadDetailSubscriptionRetention";
 import {
   canExecuteSideSlashCommand,
@@ -9184,11 +9192,31 @@ export default function ChatView({
           ...(lifecycleGeneration !== undefined ? { lifecycleGeneration } : {}),
           createdAt: new Date().toISOString(),
         })
-        .catch((err: unknown) => {
+        .catch(async (err: unknown) => {
+          if (
+            collectErrorMessages(err).some((message) =>
+              message.includes(APPROVAL_ALREADY_ANSWERED_INVARIANT_MARKER),
+            )
+          ) {
+            // The authoritative response won the race. Force a full detail
+            // snapshot so a stale local card cannot immediately submit again.
+            clearThreadDetailResumeCursor(activeThreadId);
+            await api.orchestration
+              .subscribeThread(buildThreadSubscribeInput(activeThreadId))
+              .catch(() => {
+                setStoreThreadError(
+                  activeThreadId,
+                  "Approval was already recorded, but the conversation could not be refreshed.",
+                );
+              });
+            return;
+          }
           setStoreThreadError(
             activeThreadId,
-            err instanceof Error ? err.message : "Failed to submit approval decision.",
+            describeErrorMessage(err, "Failed to submit approval decision."),
           );
+          setRespondingRequestKeys((existing) => existing.filter((key) => key !== requestKey));
+          throw err;
         });
       setRespondingRequestKeys((existing) => existing.filter((key) => key !== requestKey));
     },

apps/web/src/pendingInteractionDerivation.ts
@@ -5,18 +5,22 @@ import {
   type TurnId,
   type UserInputQuestion,
 } from "@synara/contracts";
-import { isPendingInteractionResponseClaimable } from "@synara/shared/pendingInteractions";
+import {
+  createStalePendingInteractionMatcher,
+  isPendingInteractionResponseClaimable,
+} from "@synara/shared/pendingInteractions";
 import {
   approvalRequestKindFromRequestType,
   pendingRequestInstanceKey,
 } from "@synara/shared/threadSummary";
 
-import { isStalePendingRequestFailureDetail } from "./lib/pendingInteraction";
 import { orderedActivities } from "./workLog";
 
 export interface PendingApproval {
   requestId: ApprovalRequestId;
   lifecycleGeneration?: string;
+  /** Changes only when the durable retryable response attempt changes. */
+  responseAttemptKey?: string;
   requestKind: "command" | "file-read" | "file-change" | "permissions";
   createdAt: string;
   detail?: string;
@@ -51,7 +55,6 @@ interface PendingInteractionReplay<T extends { requestId: ApprovalRequestId }> {
   interactionKind: PendingInteractionKind;
   requestedActivityKind: string;
   resolvedActivityKind: string;
-  responseFailedActivityKind: string;
   parseRequested: (input: {
     activity: OrchestrationThreadActivity;
     payload: Record<string, unknown> | null;
@@ -131,7 +134,9 @@ function retainActionableSettlements<T extends { requestId: ApprovalRequestId }>
   }
 }
 
-function replayPendingInteractions<T extends { requestId: ApprovalRequestId; createdAt: string }>(
+function replayPendingInteractions<
+  T extends { requestId: ApprovalRequestId; createdAt: string; lifecycleGeneration?: string },
+>(
   activities: ReadonlyArray<OrchestrationThreadActivity>,
   settlements: ReadonlyArray<OrchestrationPendingInteraction> | undefined,
   replay: PendingInteractionReplay<T>,
@@ -185,16 +190,19 @@ function replayPendingInteractions<T extends { requestId: ApprovalRequestId; cre
       deletePendingInteraction(openByInstance, requestId, lifecycleGeneration);
       continue;
     }
-
-    const detail = typeof payload?.detail === "string" ? payload.detail : undefined;
-    if (
-      activity.kind === replay.responseFailedActivityKind &&
-      isStalePendingRequestFailureDetail(detail)
-    ) {
-      deletePendingInteraction(openByInstance, requestId, lifecycleGeneration);
-    }
   }
 
+  // Explicit stale-callback failures are terminal for their request instance.
+  // Apply them after replay: their orchestration sequence may be below an older
+  // request's runtime sequence, which must not resurrect an invalid callback.
+  if (openByInstance.size > 0) {
+    const isStale = createStalePendingInteractionMatcher(replayActivities);
+    for (const [key, pending] of openByInstance) {
+      if (isStale({ ...pending, interactionKind: replay.interactionKind })) {
+        openByInstance.delete(key);
+      }
+    }
+  }
   retainActionableSettlements(
     openByInstance,
     settlements,
@@ -286,14 +294,13 @@ export function derivePendingApprovals(
   settlements?: ReadonlyArray<OrchestrationPendingInteraction>,
   options?: PendingInteractionDerivationOptions,
 ): PendingApproval[] {
-  return replayPendingInteractions(
+  const approvals = replayPendingInteractions(
     activities,
     settlements,
     {
       interactionKind: "approval",
       requestedActivityKind: "approval.requested",
       resolvedActivityKind: "approval.resolved",
-      responseFailedActivityKind: "provider.approval.respond.failed",
       parseRequested: ({ activity, payload, requestId, lifecycleGeneration }) => {
         const requestKind =
           payload?.requestKind === "command" ||
@@ -329,6 +336,27 @@ export function derivePendingApprovals(
     },
     options,
   );
+  if (settlements === undefined) {
+    return approvals;
+  }
+
+  const retryableAttemptKeys = new Map<string, string>();
+  for (const settlement of settlements) {
+    if (settlement.interactionKind !== "approval" || settlement.status !== "retryable") {
+      continue;
+    }
+    retryableAttemptKeys.set(
+      pendingRequestInstanceKey(settlement.requestId, settlement.lifecycleGeneration ?? undefined),
+      JSON.stringify([settlement.responseCommandId, settlement.responseRequestedAt]),
+    );
+  }
+
+  return approvals.map((approval) => {
+    const responseAttemptKey = retryableAttemptKeys.get(
+      pendingRequestInstanceKey(approval.requestId, approval.lifecycleGeneration),
+    );
+    return responseAttemptKey === undefined ? approval : { ...approval, responseAttemptKey };
+  });
 }
 
 export function derivePendingUserInputs(
@@ -343,7 +371,6 @@ export function derivePendingUserInputs(
       interactionKind: "userInput",
       requestedActivityKind: "user-input.requested",
       resolvedActivityKind: "user-input.resolved",
-      responseFailedActivityKind: "provider.user-input.respond.failed",
       parseRequested: ({ activity, payload, requestId, lifecycleGeneration }) => {
         const questions = parseUserInputQuestions(payload);
         if (!questions) {

redxzeta/forkara

apps/server/src/orchestration/commandInvariants.ts
@@ -14,7 +14,10 @@ import type {
   ThreadId,
   TurnId,
 } from "@forkara/contracts";
-import { THREAD_NOT_ARCHIVED_INVARIANT_MARKER } from "@forkara/shared/errorMessages";
+import {
+  APPROVAL_ALREADY_ANSWERED_INVARIANT_MARKER,
+  THREAD_NOT_ARCHIVED_INVARIANT_MARKER,
+} from "@forkara/shared/errorMessages";
 import {
   isLegacyHomeChatContainerRow as isSharedLegacyHomeChatContainerRow,
   isOrdinaryProjectRow as isSharedOrdinaryProjectRow,
@@ -402,7 +405,7 @@ export function requireApprovalNotResponded(input: {
   return Effect.fail(
     invariantError(
       input.command.type,
-      `Approval request '${input.requestId}' on thread '${input.threadId}' was already answered.`,
+      `Approval request '${input.requestId}' on thread '${input.threadId}' ${APPROVAL_ALREADY_ANSWERED_INVARIANT_MARKER}`,
     ),
   );
 }

apps/server/src/orchestration/startupTurnReconciliation.ts
@@ -30,12 +30,14 @@
  */
 import type {
   OrchestrationCommand,
+  OrchestrationPendingInteraction,
   OrchestrationThreadActivity,
   OrchestrationSession,
   RuntimeMode,
   ThreadId,
 } from "@forkara/contracts";
 import { CommandId, EventId } from "@forkara/contracts";
+import { createStalePendingInteractionMatcher } from "@forkara/shared/pendingInteractions";
 import {
   buildStalePendingRequestFailureDetail,
   derivePendingThreadRequestIds,
@@ -71,6 +73,14 @@ export interface ReconcilableThread {
   readonly activities?: ReadonlyArray<
     Pick<OrchestrationThreadActivity, "createdAt" | "id" | "kind" | "payload" | "sequence">
   >;
+  readonly pendingInteractions?:
+    | ReadonlyArray<
+        Pick<
+          OrchestrationPendingInteraction,
+          "interactionKind" | "requestId" | "lifecycleGeneration" | "status" | "createdAt"
+        >
+      >
+    | undefined;
 }
 
 /**
@@ -96,10 +106,39 @@ function planStalePendingRequestCommands(input: {
   readonly thread: ReconcilableThread;
   readonly now: string;
 }): ReadonlyArray<ThreadActivityAppendCommand> {
+  const commands: ThreadActivityAppendCommand[] = [];
+  if (input.thread.pendingInteractions !== undefined) {
+    const isAlreadyStale = createStalePendingInteractionMatcher(input.thread.activities ?? []);
+    for (const interaction of input.thread.pendingInteractions) {
+      // A process restart loses every live provider callback. Pending,
+      // responding, and previously retryable rows are therefore no longer
+      // answerable. Uncertain user-input responses are also retryable unless
+      // their callback has already been explicitly invalidated.
+      if (
+        interaction.status === "confirmed" ||
+        (interaction.status === "uncertain" &&
+          (interaction.interactionKind === "approval" || isAlreadyStale(interaction)))
+      ) {
+        continue;
+      }
+      commands.push(
+        buildStalePendingRequestCommand({
+          threadId: input.thread.id,
+          now: input.now,
+          requestKind: interaction.interactionKind === "approval" ? "approval" : "user-input",
+          requestId: interaction.requestId,
+          ...(interaction.lifecycleGeneration !== null
+            ? { lifecycleGeneration: interaction.lifecycleGeneration }
+            : {}),
+        }),
+      );
+    }
+    return commands;
+  }
+
   const pendingRequestIds = derivePendingThreadRequestIds({
     activities: input.thread.activities ?? [],
   });
-  const commands: ThreadActivityAppendCommand[] = [];
   for (const requestId of pendingRequestIds.approvalRequestIds) {
     commands.push(
       buildStalePendingRequestCommand({
@@ -155,6 +194,7 @@ function buildStalePendingRequestCommand(input: {
   readonly now: string;
   readonly requestKind: PendingThreadRequestKind;
   readonly requestId: string;
+  readonly lifecycleGeneration?: string;
 }): ThreadActivityAppendCommand {
   const commandKey = [
     "restart-reconcile",
@@ -178,6 +218,9 @@ function buildStalePendingRequestCommand(input: {
       payload: {
         detail: buildStalePendingRequestFailureDetail(input.requestKind, input.requestId),
         requestId: input.requestId,
+        ...(input.lifecycleGeneration !== undefined
+          ? { lifecycleGeneration: input.lifecycleGeneration }
+          : {}),
       },
       turnId: null,
       createdAt: input.now,

apps/web/src/components/ChatView.tsx
@@ -42,9 +42,14 @@ import {
   RuntimeMode,
 } from "@forkara/contracts";
 import { automationRequiresTargetThread } from "@forkara/shared/automationMode";
+import {
+  APPROVAL_ALREADY_ANSWERED_INVARIANT_MARKER,
+  collectErrorMessages,
+  describeErrorMessage,
+} from "@forkara/shared/errorMessages";
 import { respondingInteractionReclaimAt } from "@forkara/shared/pendingInteractions";
 import { providerSupportsNativeTurnSteering } from "@forkara/shared/providerMetadata";
-import { getModelCapabilities, normalizeModelSlug } from "@forkara/shared/model";
+import { getDefaultModel, getModelCapabilities, normalizeModelSlug } from "@forkara/shared/model";
 import {
   resolveLatestTailUserMessageEditTarget,
   resolveTailUserMessageEditTarget,
@@ -249,7 +254,10 @@ import {
   createComposerThreadMentionSourcesSelector,
   createThreadSelector,
 } from "../storeSelectors";
-import { buildThreadSubscribeInput } from "../threadDetailResumeCursors";
+import {
+  buildThreadSubscribeInput,
+  clearThreadDetailResumeCursor,
+} from "../threadDetailResumeCursors";
 import { retainThreadDetailSubscription } from "../threadDetailSubscriptionRetention";
 import {
   canOfferForkSlashCommand,
@@ -6422,11 +6430,31 @@ export default function ChatView({
           ...(lifecycleGeneration !== undefined ? { lifecycleGeneration } : {}),
           createdAt: new Date().toISOString(),
         })
-        .catch((err: unknown) => {
+        .catch(async (err: unknown) => {
+          if (
+            collectErrorMessages(err).some((message) =>
+              message.includes(APPROVAL_ALREADY_ANSWERED_INVARIANT_MARKER),
+            )
+          ) {
+            // The authoritative response won the race. Force a full detail
+            // snapshot so a stale local card cannot immediately submit again.
+            clearThreadDetailResumeCursor(activeThreadId);
+            await api.orchestration
+              .subscribeThread(buildThreadSubscribeInput(activeThreadId))
+              .catch(() => {
+                setStoreThreadError(
+                  activeThreadId,
+                  "Approval was already recorded, but the conversation could not be refreshed.",
+                );
+              });
+            return;
+          }
           setStoreThreadError(
             activeThreadId,
-            err instanceof Error ? err.message : "Failed to submit approval decision.",
+            describeErrorMessage(err, "Failed to submit approval decision."),
           );
+          setRespondingRequestKeys((existing) => existing.filter((key) => key !== requestKey));
+          throw err;
         });
       setRespondingRequestKeys((existing) => existing.filter((key) => key !== requestKey));
     },

apps/web/src/pendingInteractionDerivation.ts
@@ -5,18 +5,22 @@ import {
   type TurnId,
   type UserInputQuestion,
 } from "@forkara/contracts";
-import { isPendingInteractionResponseClaimable } from "@forkara/shared/pendingInteractions";
+import {
+  createStalePendingInteractionMatcher,
+  isPendingInteractionResponseClaimable,
+} from "@forkara/shared/pendingInteractions";
 import {
   approvalRequestKindFromRequestType,
   pendingRequestInstanceKey,
 } from "@forkara/shared/threadSummary";
 
-import { isStalePendingRequestFailureDetail } from "./lib/pendingInteraction";
 import { orderedActivities } from "./workLog";
 
 export interface PendingApproval {
   requestId: ApprovalRequestId;
   lifecycleGeneration?: string;
+  /** Changes only when the durable retryable response attempt changes. */
+  responseAttemptKey?: string;
   requestKind: "command" | "file-read" | "file-change" | "permissions";
   createdAt: string;
   detail?: string;
@@ -51,7 +55,6 @@ interface PendingInteractionReplay<T extends { requestId: ApprovalRequestId }> {
   interactionKind: PendingInteractionKind;
   requestedActivityKind: string;
   resolvedActivityKind: string;
-  responseFailedActivityKind: string;
   parseRequested: (input: {
     activity: OrchestrationThreadActivity;
     payload: Record<string, unknown> | null;
@@ -131,7 +134,9 @@ function retainActionableSettlements<T extends { requestId: ApprovalRequestId }>
   }
 }
 
-function replayPendingInteractions<T extends { requestId: ApprovalRequestId; createdAt: string }>(
+function replayPendingInteractions<
+  T extends { requestId: ApprovalRequestId; createdAt: string; lifecycleGeneration?: string },
+>(
   activities: ReadonlyArray<OrchestrationThreadActivity>,
   settlements: ReadonlyArray<OrchestrationPendingInteraction> | undefined,
   replay: PendingInteractionReplay<T>,
@@ -185,16 +190,19 @@ function replayPendingInteractions<T extends { requestId: ApprovalRequestId; cre
       deletePendingInteraction(openByInstance, requestId, lifecycleGeneration);
       continue;
     }
-
-    const detail = typeof payload?.detail === "string" ? payload.detail : undefined;
-    if (
-      activity.kind === replay.responseFailedActivityKind &&
-      isStalePendingRequestFailureDetail(detail)
-    ) {
-      deletePendingInteraction(openByInstance, requestId, lifecycleGeneration);
-    }
   }
 
+  // Explicit stale-callback failures are terminal for their request instance.
+  // Apply them after replay: their orchestration sequence may be below an older
+  // request's runtime sequence, which must not resurrect an invalid callback.
+  if (openByInstance.size > 0) {
+    const isStale = createStalePendingInteractionMatcher(replayActivities);
+    for (const [key, pending] of openByInstance) {
+      if (isStale({ ...pending, interactionKind: replay.interactionKind })) {
+        openByInstance.delete(key);
+      }
+    }
+  }
   retainActionableSettlements(
     openByInstance,
     settlements,
@@ -286,14 +294,13 @@ export function derivePendingApprovals(
   settlements?: ReadonlyArray<OrchestrationPendingInteraction>,
   options?: PendingInteractionDerivationOptions,
 ): PendingApproval[] {
-  return replayPendingInteractions(
+  const approvals = replayPendingInteractions(
     activities,
     settlements,
     {
       interactionKind: "approval",
       requestedActivityKind: "approval.requested",
       resolvedActivityKind: "approval.resolved",
-      responseFailedActivityKind: "provider.approval.respond.failed",
       parseRequested: ({ activity, payload, requestId, lifecycleGeneration }) => {
         const requestKind =
           payload?.requestKind === "command" ||
@@ -329,6 +336,27 @@ export function derivePendingApprovals(
     },
     options,
   );
+  if (settlements === undefined) {
+    return approvals;
+  }
+
+  const retryableAttemptKeys = new Map<string, string>();
+  for (const settlement of settlements) {
+    if (settlement.interactionKind !== "approval" || settlement.status !== "retryable") {
+      continue;
+    }
+    retryableAttemptKeys.set(
+      pendingRequestInstanceKey(settlement.requestId, settlement.lifecycleGeneration ?? undefined),
+      JSON.stringify([settlement.responseCommandId, settlement.responseRequestedAt]),
+    );
+  }
+
+  return approvals.map((approval) => {
+    const responseAttemptKey = retryableAttemptKeys.get(
+      pendingRequestInstanceKey(approval.requestId, approval.lifecycleGeneration),
+    );
+    return responseAttemptKey === undefined ? approval : { ...approval, responseAttemptKey };
+  });
 }
 
 export function derivePendingUserInputs(
@@ -343,7 +371,6 @@ export function derivePendingUserInputs(
       interactionKind: "userInput",
       requestedActivityKind: "user-input.requested",
       resolvedActivityKind: "user-input.resolved",
-      responseFailedActivityKind: "provider.user-input.respond.failed",
       parseRequested: ({ activity, payload, requestId, lifecycleGeneration }) => {
         const questions = parseUserInputQuestions(payload);
         if (!questions) {