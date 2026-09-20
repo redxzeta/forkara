Neonity2020/synara

Keep Codex Markdown together across text segments (#1155)
Keep invisible provider events from splitting assistant Markdown and coalesce adjacent saved segments while preserving real timeline boundaries.

Preserve the partially visible chat row during streaming and repair shared CI timing and default-model expectations. All 22 PR checks passed and Cursor Bugbot reported zero findings.

apps/server/src/orchestration/Layers/messageTextChunks.test.ts
@@ -135,6 +135,34 @@ async function assertReaders(system: Awaited<ReturnType<typeof openSystem>>, tex
   return row;
 }
 
+it("stores contiguous CJK token deltas as one segment across reload and settlement", async () => {
+  const dir = await mkdtemp(join(tmpdir(), "synara-cjk-chunks-"));
+  let system = await openSystem(dir);
+  const text = "知道。\n\n- 前端 Web：`/project/web`\n- `erp-code` 是 ERP 项目。";
+  try {
+    await system.seed();
+    for (const [index, delta] of Array.from(text).entries()) {
+      await system.delta(`cjk-${index}`, delta);
+    }
+    const streaming = await assertReaders(system, text);
+    expect(streaming.textSegments).toHaveLength(1);
+    expect(
+      await system.run(system.sql`SELECT COUNT(*) AS count FROM message_text_segments`),
+    ).toEqual([{ count: 1 }]);
+    await system.runtime.dispose();
+    system = await openSystem(dir);
+    expect((await assertReaders(system, text)).textSegments).toHaveLength(1);
+    await system.complete();
+    expect((await assertReaders(system, text)).textSegments).toBeUndefined();
+    await system.runtime.dispose();
+    system = await openSystem(dir);
+    expect((await assertReaders(system, text)).textSegments).toBeUndefined();
+  } finally {
+    await system.runtime.dispose();
+    await rm(dir, { recursive: true, force: true });
+  }
+});
+
 it("preserves split Unicode across segments, restart and completion in every reader", async () => {
   const dir = await mkdtemp(join(tmpdir(), "synara-chunk-restart-"));
   let system = await openSystem(dir);

apps/web/src/components/chat/MessagesTimeline.segments.browser.tsx
@@ -1,0 +1,66 @@
+import "../../index.css";
+
+import { MessageId } from "@synara/contracts";
+import { afterEach, expect, it } from "vitest";
+import { render } from "vitest-browser-react";
+import type { ChatMessage } from "../../types";
+import { deriveTimelineEntries } from "../../workLog";
+import { MessagesTimeline } from "./MessagesTimeline";
+
+const now = "2026-09-12T12:00:00.000Z";
+const text = "知道。\n\n- 前端 Web 项目：`/project/web`\n- `erp-code` 通常指 C# ERP 项目。";
+const message: ChatMessage = {
+  id: MessageId.makeUnsafe("assistant-cjk"),
+  role: "assistant",
+  text,
+  createdAt: now,
+  streaming: false,
+  textSegments: Array.from(text, (text, index) => ({
+    sequence: index + 1,
+    startedAt: now,
+    endedAt: now,
+    text,
+  })),
+};
+
+let cleanup: (() => void) | undefined;
+afterEach(() => cleanup?.());
+
+it("renders a reopened tokenized CJK reply as one intact Markdown list", async () => {
+  const incoming = JSON.parse(JSON.stringify(message)) as ChatMessage;
+  const result = await render(
+    <div style={{ height: 600 }}>
+      <MessagesTimeline
+        hasMessages
+        isWorking={false}
+        activeTurnInProgress={false}
+        activeTurnStartedAt={null}
+        timelineEntries={deriveTimelineEntries([incoming], [], [])}
+        turnDiffSummaryByAssistantMessageId={new Map()}
+        nowIso={now}
+        expandedWorkGroups={{}}
+        onToggleWorkGroup={() => {}}
+        onOpenTurnDiff={() => {}}
+        revertTurnCountByUserMessageId={new Map()}
+        onRevertUserMessage={() => {}}
+        isRevertingCheckpoint={false}
+        onImageExpand={() => {}}
+        markdownCwd={undefined}
+        resolvedTheme="dark"
+        timestampFormat="locale"
+        workspaceRoot={undefined}
+      />
+    </div>,
+  );
+  cleanup = result.unmount;
+  await expect.poll(() => document.querySelectorAll("li").length).toBe(2);
+  expect(Array.from(document.querySelectorAll("li"), (item) => item.textContent)).toEqual([
+    "前端 Web 项目：/project/web",
+    "erp-code 通常指 C# ERP 项目。",
+  ]);
+  expect(Array.from(document.querySelectorAll("code"), (item) => item.textContent)).toEqual([
+    "/project/web",
+    "erp-code",
+  ]);
+  expect(document.querySelectorAll(".chat-message-segment")).toHaveLength(0);
+});

redxzeta/forkara

apps/server/src/orchestration/Layers/messageTextChunks.test.ts
@@ -135,6 +135,34 @@ async function assertReaders(system: Awaited<ReturnType<typeof openSystem>>, tex
   return row;
 }
 
+it("stores contiguous CJK token deltas as one segment across reload and settlement", async () => {
+  const dir = await mkdtemp(join(tmpdir(), "forkara-cjk-chunks-"));
+  let system = await openSystem(dir);
+  const text = "知道。\n\n- 前端 Web：`/project/web`\n- `erp-code` 是 ERP 项目。";
+  try {
+    await system.seed();
+    for (const [index, delta] of Array.from(text).entries()) {
+      await system.delta(`cjk-${index}`, delta);
+    }
+    const streaming = await assertReaders(system, text);
+    expect(streaming.textSegments).toHaveLength(1);
+    expect(
+      await system.run(system.sql`SELECT COUNT(*) AS count FROM message_text_segments`),
+    ).toEqual([{ count: 1 }]);
+    await system.runtime.dispose();
+    system = await openSystem(dir);
+    expect((await assertReaders(system, text)).textSegments).toHaveLength(1);
+    await system.complete();
+    expect((await assertReaders(system, text)).textSegments).toBeUndefined();
+    await system.runtime.dispose();
+    system = await openSystem(dir);
+    expect((await assertReaders(system, text)).textSegments).toBeUndefined();
+  } finally {
+    await system.runtime.dispose();
+    await rm(dir, { recursive: true, force: true });
+  }
+});
+
 it("preserves split Unicode across segments, restart and completion in every reader", async () => {
   const dir = await mkdtemp(join(tmpdir(), "forkara-chunk-restart-"));
   let system = await openSystem(dir);

apps/web/src/components/chat/MessagesTimeline.segments.browser.tsx
@@ -1,0 +1,66 @@
+import "../../index.css";
+
+import { MessageId } from "@forkara/contracts";
+import { afterEach, expect, it } from "vitest";
+import { render } from "vitest-browser-react";
+import type { ChatMessage } from "../../types";
+import { deriveTimelineEntries } from "../../workLog";
+import { MessagesTimeline } from "./MessagesTimeline";
+
+const now = "2026-09-12T12:00:00.000Z";
+const text = "知道。\n\n- 前端 Web 项目：`/project/web`\n- `erp-code` 通常指 C# ERP 项目。";
+const message: ChatMessage = {
+  id: MessageId.makeUnsafe("assistant-cjk"),
+  role: "assistant",
+  text,
+  createdAt: now,
+  streaming: false,
+  textSegments: Array.from(text, (text, index) => ({
+    sequence: index + 1,
+    startedAt: now,
+    endedAt: now,
+    text,
+  })),
+};
+
+let cleanup: (() => void) | undefined;
+afterEach(() => cleanup?.());
+
+it("renders a reopened tokenized CJK reply as one intact Markdown list", async () => {
+  const incoming = JSON.parse(JSON.stringify(message)) as ChatMessage;
+  const result = await render(
+    <div style={{ height: 600 }}>
+      <MessagesTimeline
+        hasMessages
+        isWorking={false}
+        activeTurnInProgress={false}
+        activeTurnStartedAt={null}
+        timelineEntries={deriveTimelineEntries([incoming], [], [])}
+        turnDiffSummaryByAssistantMessageId={new Map()}
+        nowIso={now}
+        expandedWorkGroups={{}}
+        onToggleWorkGroup={() => {}}
+        onOpenTurnDiff={() => {}}
+        revertTurnCountByUserMessageId={new Map()}
+        onRevertUserMessage={() => {}}
+        isRevertingCheckpoint={false}
+        onImageExpand={() => {}}
+        markdownCwd={undefined}
+        resolvedTheme="dark"
+        timestampFormat="locale"
+        workspaceRoot={undefined}
+      />
+    </div>,
+  );
+  cleanup = result.unmount;
+  await expect.poll(() => document.querySelectorAll("li").length).toBe(2);
+  expect(Array.from(document.querySelectorAll("li"), (item) => item.textContent)).toEqual([
+    "前端 Web 项目：/project/web",
+    "erp-code 通常指 C# ERP 项目。",
+  ]);
+  expect(Array.from(document.querySelectorAll("code"), (item) => item.textContent)).toEqual([
+    "/project/web",
+    "erp-code",
+  ]);
+  expect(document.querySelectorAll(".chat-message-segment")).toHaveLength(0);
+});