Emanuele-web04/synara

[codex] Fix Fast mode reset and duplicate skill input (#1125)
* fix(codex): reset Fast mode and avoid duplicate skill input

* test(web): align current UI expectations

apps/server/src/codexAppServerManager.test.ts
@@ -2986,39 +2986,61 @@ describe("thread checkpoint control", () => {
     });
   });
 
-  it.skipIf(!process.env.CODEX_BINARY_PATH)("forks a provider thread via thread/fork", async () => {
+  it("forks a provider thread with an explicitly selected Standard tier", async () => {
+    const homePath = mkdtempSync(path.join(os.tmpdir(), "synara-codex-fork-tier-"));
+    writeFileSync(path.join(homePath, "app-server"), "process.stdin.resume();\n");
+    const previousSynaraHome = process.env.SYNARA_HOME;
+    process.env.SYNARA_HOME = path.join(homePath, "synara-home");
     const { manager, sendRequest } = createThreadControlHarness();
+    vi.spyOn(
+      manager as unknown as { assertSupportedCodexCliVersion: () => Promise<void> },
+      "assertSupportedCodexCliVersion",
+    ).mockResolvedValue(undefined);
     sendRequest.mockResolvedValue({
       thread: {
         id: "thread_forked",
       },
     });
 
-    const result = await manager.forkThread({
-      sourceThreadId: asThreadId("thread_1"),
-      sourceResumeCursor: {
-        threadId: "thread_1",
-      },
-      threadId: asThreadId("thread_2"),
-      runtimeMode: "full-access",
-    });
+    try {
+      const result = await manager.forkThread({
+        sourceThreadId: asThreadId("thread_1"),
+        sourceResumeCursor: {
+          threadId: "thread_1",
+        },
+        threadId: asThreadId("thread_2"),
+        cwd: homePath,
+        providerOptions: { codex: { binaryPath: process.execPath, homePath } },
+        modelSelection: {
+          provider: "codex",
+          model: "gpt-5.4",
+          options: { fastMode: false },
+        },
+        runtimeMode: "full-access",
+      });
 
-    expect(sendRequest).toHaveBeenNthCalledWith(
-      3,
-      expect.anything(),
-      "thread/fork",
-      expect.objectContaining({
+      const forkRequest = sendRequest.mock.calls.find(([, method]) => method === "thread/fork");
+      expect(forkRequest?.[2]).toMatchObject({
         threadId: "thread_1",
+        serviceTier: "default",
         approvalPolicy: "never",
         sandbox: "danger-full-access",
-      }),
-    );
-    expect(result).toEqual({
-      threadId: "thread_2",
-      resumeCursor: {
-        threadId: "thread_forked",
-      },
-    });
+      });
+      expect(result).toEqual({
+        threadId: "thread_2",
+        resumeCursor: {
+          threadId: "thread_forked",
+        },
+      });
+    } finally {
+      await manager.stopAll();
+      if (previousSynaraHome === undefined) {
+        delete process.env.SYNARA_HOME;
+      } else {
+        process.env.SYNARA_HOME = previousSynaraHome;
+      }
+      rmSync(homePath, { recursive: true, force: true });
+    }
   });
 
   it("rolls back turns via thread/rollback and resets session running state", async () => {

apps/server/src/codexServiceTier.ts
@@ -1,0 +1,12 @@
+import type { ModelSelection } from "@synara/contracts";
+
+export function resolveCodexServiceTier(
+  modelSelection: ModelSelection | undefined,
+): "fast" | "default" | undefined {
+  if (modelSelection?.provider !== "codex" || modelSelection.options?.fastMode === undefined) {
+    return undefined;
+  }
+
+  // Omitting the tier preserves Codex's previous value, including Fast mode.
+  return modelSelection.options.fastMode ? "fast" : "default";
+}

apps/server/src/provider/skillPromptInjection.test.ts
@@ -28,10 +28,12 @@ const codeiumSkillPath = "/Users/me/.codeium/windsurf/skills/reviewer/SKILL.md";
 
 describe("shouldInlineSkillForProvider", () => {
   it("skips codex-native and synara roots for codex but inlines foreign provider roots", () => {
-    // Codex loads .codex roots natively and ~/.synara/skills via the extra
-    // skill root registered at session start.
     expect(shouldInlineSkillForProvider("codex", synaraSkillPath)).toBe(false);
     expect(shouldInlineSkillForProvider("codex", codexSkillPath)).toBe(false);
+    expect(shouldInlineSkillForProvider("codex", agentsSkillPath)).toBe(false);
+    expect(shouldInlineSkillForProvider("codex", "/repo/.agents/skills/reviewer/SKILL.md")).toBe(
+      false,
+    );
     expect(shouldInlineSkillForProvider("codex", claudeSkillPath)).toBe(true);
     expect(shouldInlineSkillForProvider("codex", cursorSkillPath)).toBe(true);
   });
@@ -125,12 +127,25 @@ describe("buildInlineSkillInstructions", () => {
     }
   });
 
-  it("does not inline synara-rooted skills for codex (covered by the extra skill root)", async () => {
-    const text = await buildInlineSkillInstructions({
-      provider: "codex",
-      skills: [{ name: "reviewer", path: synaraSkillPath }],
-      maxChars: 10_000,
-    });
-    expect(text).toBe("");
-  });
+  it.each([".synara", ".agents"])(
+    "does not duplicate %s skill instructions loaded natively by Codex",
+    async (skillRoot) => {
+      const root = mkdtempSync(path.join(os.tmpdir(), "skill-native-"));
+      const skillDir = path.join(root, skillRoot, "skills", "reviewer");
+      try {
+        await mkdir(skillDir, { recursive: true });
+        const skillPath = path.join(skillDir, "SKILL.md");
+        await writeFile(skillPath, "# Reviewer\n\nAlways review carefully.");
+
+        const text = await buildInlineSkillInstructions({
+          provider: "codex",
+          skills: [{ name: "reviewer", path: skillPath }],
+          maxChars: 10_000,
+        });
+        expect(text).toBe("");
+      } finally {
+        rmSync(root, { recursive: true, force: true });
+      }
+    },
+  );
 });

redxzeta/forkara

apps/server/src/codexAppServerManager.test.ts
@@ -2944,39 +2944,61 @@ describe("thread checkpoint control", () => {
     });
   });
 
-  it.skipIf(!process.env.CODEX_BINARY_PATH)("forks a provider thread via thread/fork", async () => {
+  it("forks a provider thread with an explicitly selected Standard tier", async () => {
+    const homePath = mkdtempSync(path.join(os.tmpdir(), "forkara-codex-fork-tier-"));
+    writeFileSync(path.join(homePath, "app-server"), "process.stdin.resume();\n");
+    const previousForkaraHome = process.env.FORKARA_HOME;
+    process.env.FORKARA_HOME = path.join(homePath, "forkara-home");
     const { manager, sendRequest } = createThreadControlHarness();
+    vi.spyOn(
+      manager as unknown as { assertSupportedCodexCliVersion: () => Promise<void> },
+      "assertSupportedCodexCliVersion",
+    ).mockResolvedValue(undefined);
     sendRequest.mockResolvedValue({
       thread: {
         id: "thread_forked",
       },
     });
 
-    const result = await manager.forkThread({
-      sourceThreadId: asThreadId("thread_1"),
-      sourceResumeCursor: {
-        threadId: "thread_1",
-      },
-      threadId: asThreadId("thread_2"),
-      runtimeMode: "full-access",
-    });
+    try {
+      const result = await manager.forkThread({
+        sourceThreadId: asThreadId("thread_1"),
+        sourceResumeCursor: {
+          threadId: "thread_1",
+        },
+        threadId: asThreadId("thread_2"),
+        cwd: homePath,
+        providerOptions: { codex: { binaryPath: process.execPath, homePath } },
+        modelSelection: {
+          provider: "codex",
+          model: "gpt-5.4",
+          options: { fastMode: false },
+        },
+        runtimeMode: "full-access",
+      });
 
-    expect(sendRequest).toHaveBeenNthCalledWith(
-      3,
-      expect.anything(),
-      "thread/fork",
-      expect.objectContaining({
+      const forkRequest = sendRequest.mock.calls.find(([, method]) => method === "thread/fork");
+      expect(forkRequest?.[2]).toMatchObject({
         threadId: "thread_1",
+        serviceTier: "default",
         approvalPolicy: "never",
         sandbox: "danger-full-access",
-      }),
-    );
-    expect(result).toEqual({
-      threadId: "thread_2",
-      resumeCursor: {
-        threadId: "thread_forked",
-      },
-    });
+      });
+      expect(result).toEqual({
+        threadId: "thread_2",
+        resumeCursor: {
+          threadId: "thread_forked",
+        },
+      });
+    } finally {
+      await manager.stopAll();
+      if (previousForkaraHome === undefined) {
+        delete process.env.FORKARA_HOME;
+      } else {
+        process.env.FORKARA_HOME = previousForkaraHome;
+      }
+      rmSync(homePath, { recursive: true, force: true });
+    }
   });
 
   it("rolls back turns via thread/rollback and resets session running state", async () => {

apps/server/src/codexServiceTier.ts
@@ -1,0 +1,12 @@
+import type { ModelSelection } from "@forkara/contracts";
+
+export function resolveCodexServiceTier(
+  modelSelection: ModelSelection | undefined,
+): "fast" | "default" | undefined {
+  if (modelSelection?.provider !== "codex" || modelSelection.options?.fastMode === undefined) {
+    return undefined;
+  }
+
+  // Omitting the tier preserves Codex's previous value, including Fast mode.
+  return modelSelection.options.fastMode ? "fast" : "default";
+}

apps/server/src/provider/skillPromptInjection.test.ts
@@ -19,6 +19,7 @@ const forkaraSkillPath = "/Users/me/.forkara/skills/reviewer/SKILL.md";
 const codexSkillPath = "/Users/me/.codex/skills/reviewer/SKILL.md";
 const claudeSkillPath = "/Users/me/.claude/skills/reviewer/SKILL.md";
 const cursorSkillPath = "/Users/me/.cursor/skills/reviewer/SKILL.md";
+const agentsSkillPath = "/Users/me/.agents/skills/reviewer/SKILL.md";
 const piSkillPath = "/Users/me/.pi/agent/skills/reviewer/SKILL.md";
 
 describe("shouldInlineSkillForProvider", () => {
@@ -27,6 +28,10 @@ describe("shouldInlineSkillForProvider", () => {
     // skill root registered at session start.
     expect(shouldInlineSkillForProvider("codex", forkaraSkillPath)).toBe(false);
     expect(shouldInlineSkillForProvider("codex", codexSkillPath)).toBe(false);
+    expect(shouldInlineSkillForProvider("codex", agentsSkillPath)).toBe(false);
+    expect(shouldInlineSkillForProvider("codex", "/repo/.agents/skills/reviewer/SKILL.md")).toBe(
+      false,
+    );
     expect(shouldInlineSkillForProvider("codex", claudeSkillPath)).toBe(true);
     expect(shouldInlineSkillForProvider("codex", cursorSkillPath)).toBe(true);
   });
@@ -103,12 +108,25 @@ describe("buildInlineSkillInstructions", () => {
     }
   });
 
-  it("does not inline forkara-rooted skills for codex (covered by the extra skill root)", async () => {
-    const text = await buildInlineSkillInstructions({
-      provider: "codex",
-      skills: [{ name: "reviewer", path: forkaraSkillPath }],
-      maxChars: 10_000,
-    });
-    expect(text).toBe("");
-  });
+  it.each([".forkara", ".agents"])(
+    "does not duplicate %s skill instructions loaded natively by Codex",
+    async (skillRoot) => {
+      const root = mkdtempSync(path.join(os.tmpdir(), "skill-native-"));
+      const skillDir = path.join(root, skillRoot, "skills", "reviewer");
+      try {
+        await mkdir(skillDir, { recursive: true });
+        const skillPath = path.join(skillDir, "SKILL.md");
+        await writeFile(skillPath, "# Reviewer\n\nAlways review carefully.");
+
+        const text = await buildInlineSkillInstructions({
+          provider: "codex",
+          skills: [{ name: "reviewer", path: skillPath }],
+          maxChars: 10_000,
+        });
+        expect(text).toBe("");
+      } finally {
+        rmSync(root, { recursive: true, force: true });
+      }
+    },
+  );
 });