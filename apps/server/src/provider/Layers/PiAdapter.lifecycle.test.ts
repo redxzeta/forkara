Emanuele-web04/synara

fix(pi): keep extension status out of tool timeline (#1093)
* fix(pi): keep extension status out of tool timeline

* Preserve Pi notifications and plain text while filtering terminal status

* Use the approval request ID schema in the Pi input test

---------

Co-authored-by: Emanuele Di Pietro <127427797+Emanuele-web04@users.noreply.github.com>

apps/server/src/provider/Layers/PiAdapter.lifecycle.test.ts
@@ -10,7 +10,12 @@ import type {
   InlineExtension,
 } from "@earendil-works/pi-coding-agent";
 import { Effect, Layer, Stream } from "effect";
-import { ThreadId, type ProviderRuntimeEvent, type TurnId } from "@synara/contracts";
+import {
+  ApprovalRequestId,
+  ThreadId,
+  type ProviderRuntimeEvent,
+  type TurnId,
+} from "@synara/contracts";
 import { afterEach, expect, it, vi } from "vitest";
 import {
   AgentGatewayCredentials,
@@ -274,6 +279,86 @@ async function expectNextTurn(
   );
   expect(completions(events).filter((event) => event.turnId === previous)).toHaveLength(1);
 }
+
+it("does not turn extension footer status into transcript tool progress", async () => {
+  responses("success");
+  captured.extensions.push((pi) => {
+    pi.on("agent_start", (_event, context) => {
+      context.ui.setStatus("caveman", "\u001b[38;2;215;119;87m⠠\u001b[0m caveman level: FULL");
+      context.ui.setStatus("caveman", "\u001b[38;2;215;119;87m⠔\u001b[0m caveman level: FULL");
+      context.ui.setWorkingMessage("Working...");
+      context.ui.setTitle("Pi terminal");
+    });
+  });
+
+  await withAdapter(async (adapter, events) => {
+    await send(adapter);
+    await waitFor(() => expect(completions(events)).toHaveLength(1));
+    expect(events.filter((event) => event.type === "tool.progress")).toHaveLength(0);
+  });
+});
+
+it("keeps extension notifications visible without inventing tool progress", async () => {
+  responses("success");
+  captured.extensions.push((pi) => {
+    pi.on("agent_start", (_event, context) => {
+      context.ui.notify("\u001b[31mEnabled [full] mode\u001b[0m");
+      context.ui.notify("Configuration saved", "info");
+      context.ui.notify("Please reconnect", "warning");
+      context.ui.notify("Tool failed", "error");
+    });
+  });
+
+  await withAdapter(async (adapter, events) => {
+    await send(adapter);
+    await waitFor(() => expect(completions(events)).toHaveLength(1));
+    const notices = events.filter((event) => event.raw?.method === "extension/ui/notify");
+    expect(notices.map((event) => ({ type: event.type, payload: event.payload }))).toEqual([
+      {
+        type: "runtime.warning",
+        payload: { message: "Enabled [full] mode", detail: { type: "info" } },
+      },
+      {
+        type: "runtime.warning",
+        payload: { message: "Configuration saved", detail: { type: "info" } },
+      },
+      {
+        type: "runtime.warning",
+        payload: { message: "Please reconnect", detail: { type: "warning" } },
+      },
+      { type: "runtime.warning", payload: { message: "Tool failed", detail: { type: "error" } } },
+    ]);
+    expect(events.filter((event) => event.type === "tool.progress")).toHaveLength(0);
+  });
+});
+
+it("cleans input prompt formatting while preserving the user's answer", async () => {
+  responses("success");
+  const answer = "items[0]; \u001b[31m";
+  let received: string | undefined;
+  captured.extensions.push((pi) => {
+    pi.on("agent_start", async (_event, context) => {
+      received = await context.ui.input("\u001b[31mExpression [code]\u001b[0m");
+    });
+  });
+
+  await withAdapter(async (adapter, events) => {
+    const sent = send(adapter);
+    await waitFor(() =>
+      expect(events.some((event) => event.type === "user-input.requested")).toBe(true),
+    );
+    const request = events.find((event) => event.type === "user-input.requested")!;
+    expect(request.payload.questions[0]?.question).toBe("Expression [code]");
+    await Effect.runPromise(
+      adapter.respondToUserInput(threadId, ApprovalRequestId.makeUnsafe(request.requestId!), {
+        input: answer,
+      }),
+    );
+    await sent;
+    await waitFor(() => expect(completions(events)).toHaveLength(1));
+    expect(received).toBe(answer);
+  });
+});
 
 it("keeps one turn through a real SDK retry and settles text, reasoning and usage once", async () => {
   const calls = responses("error", "success");

apps/web/src/workLog.ts
@@ -21,7 +21,7 @@ import {
   stripTrailingToolExitCode,
   summarizeToolRawOutput,
 } from "@synara/shared/toolOutputSummary";
-import { pluralize } from "@synara/shared/text";
+import { pluralize, stripTerminalControlSequences } from "@synara/shared/text";
 import { PROVIDER_DESCRIPTORS } from "@synara/shared/providerMetadata";
 import {
   deriveReadableToolTitle,
@@ -602,7 +602,7 @@ function toDerivedWorkLogEntry(activity: OrchestrationThreadActivity): DerivedWo
   const itemType = extractWorkLogItemType(payload);
   const requestKind = extractWorkLogRequestKind(payload);
   if (payload && typeof payload.detail === "string" && payload.detail.length > 0) {
-    const detail = stripTrailingExitCode(payload.detail).output;
+    const detail = stripTrailingExitCode(stripTerminalControlSequences(payload.detail)).output;
     if (detail) {
       entry.detail = detail;
     }
@@ -610,11 +610,11 @@ function toDerivedWorkLogEntry(activity: OrchestrationThreadActivity): DerivedWo
   const outputDetail =
     activity.kind === "provider.event.unmapped" ? null : summarizeToolPayloadOutput(payload);
   if (outputDetail && (!entry.detail || toolStatus === "failed")) {
-    entry.detail = outputDetail;
+    entry.detail = stripTerminalControlSequences(outputDetail);
   }
   const collabTaskOutputDetail = extractCollabTaskOutputDetail(payload);
   if (collabTaskOutputDetail) {
-    entry.detail = collabTaskOutputDetail;
+    entry.detail = stripTerminalControlSequences(collabTaskOutputDetail);
   }
   const nativeEventType =
     payload && typeof payload.nativeEventType === "string" && payload.nativeEventType.length > 0
@@ -627,7 +627,7 @@ function toDerivedWorkLogEntry(activity: OrchestrationThreadActivity): DerivedWo
     activity.kind === "runtime.warning" &&
     typeof payload?.message === "string" &&
     payload.message.trim().length > 0
-      ? payload.message.trim()
+      ? stripTerminalControlSequences(payload.message).trim()
       : undefined;
   if (runtimeWarningMessage) {
     entry.detail = runtimeWarningMessage;

redxzeta/forkara

apps/server/src/provider/Layers/PiAdapter.lifecycle.test.ts
@@ -10,7 +10,12 @@ import type {
   InlineExtension,
 } from "@earendil-works/pi-coding-agent";
 import { Effect, Layer, Stream } from "effect";
-import { ThreadId, type ProviderRuntimeEvent, type TurnId } from "@forkara/contracts";
+import {
+  ApprovalRequestId,
+  ThreadId,
+  type ProviderRuntimeEvent,
+  type TurnId,
+} from "@forkara/contracts";
 import { afterEach, expect, it, vi } from "vitest";
 import {
   AgentGatewayCredentials,
@@ -214,6 +219,86 @@ async function expectNextTurn(
   );
   expect(completions(events).filter((event) => event.turnId === previous)).toHaveLength(1);
 }
+
+it("does not turn extension footer status into transcript tool progress", async () => {
+  responses("success");
+  captured.extensions.push((pi) => {
+    pi.on("agent_start", (_event, context) => {
+      context.ui.setStatus("caveman", "\u001b[38;2;215;119;87m⠠\u001b[0m caveman level: FULL");
+      context.ui.setStatus("caveman", "\u001b[38;2;215;119;87m⠔\u001b[0m caveman level: FULL");
+      context.ui.setWorkingMessage("Working...");
+      context.ui.setTitle("Pi terminal");
+    });
+  });
+
+  await withAdapter(async (adapter, events) => {
+    await send(adapter);
+    await waitFor(() => expect(completions(events)).toHaveLength(1));
+    expect(events.filter((event) => event.type === "tool.progress")).toHaveLength(0);
+  });
+});
+
+it("keeps extension notifications visible without inventing tool progress", async () => {
+  responses("success");
+  captured.extensions.push((pi) => {
+    pi.on("agent_start", (_event, context) => {
+      context.ui.notify("\u001b[31mEnabled [full] mode\u001b[0m");
+      context.ui.notify("Configuration saved", "info");
+      context.ui.notify("Please reconnect", "warning");
+      context.ui.notify("Tool failed", "error");
+    });
+  });
+
+  await withAdapter(async (adapter, events) => {
+    await send(adapter);
+    await waitFor(() => expect(completions(events)).toHaveLength(1));
+    const notices = events.filter((event) => event.raw?.method === "extension/ui/notify");
+    expect(notices.map((event) => ({ type: event.type, payload: event.payload }))).toEqual([
+      {
+        type: "runtime.warning",
+        payload: { message: "Enabled [full] mode", detail: { type: "info" } },
+      },
+      {
+        type: "runtime.warning",
+        payload: { message: "Configuration saved", detail: { type: "info" } },
+      },
+      {
+        type: "runtime.warning",
+        payload: { message: "Please reconnect", detail: { type: "warning" } },
+      },
+      { type: "runtime.warning", payload: { message: "Tool failed", detail: { type: "error" } } },
+    ]);
+    expect(events.filter((event) => event.type === "tool.progress")).toHaveLength(0);
+  });
+});
+
+it("cleans input prompt formatting while preserving the user's answer", async () => {
+  responses("success");
+  const answer = "items[0]; \u001b[31m";
+  let received: string | undefined;
+  captured.extensions.push((pi) => {
+    pi.on("agent_start", async (_event, context) => {
+      received = await context.ui.input("\u001b[31mExpression [code]\u001b[0m");
+    });
+  });
+
+  await withAdapter(async (adapter, events) => {
+    const sent = send(adapter);
+    await waitFor(() =>
+      expect(events.some((event) => event.type === "user-input.requested")).toBe(true),
+    );
+    const request = events.find((event) => event.type === "user-input.requested")!;
+    expect(request.payload.questions[0]?.question).toBe("Expression [code]");
+    await Effect.runPromise(
+      adapter.respondToUserInput(threadId, ApprovalRequestId.makeUnsafe(request.requestId!), {
+        input: answer,
+      }),
+    );
+    await sent;
+    await waitFor(() => expect(completions(events)).toHaveLength(1));
+    expect(received).toBe(answer);
+  });
+});
 
 it("keeps one turn through a real SDK retry and settles text, reasoning and usage once", async () => {
   const calls = responses("error", "success");

apps/web/src/workLog.ts
@@ -22,7 +22,7 @@ import {
   stripTrailingToolExitCode,
   summarizeToolRawOutput,
 } from "@forkara/shared/toolOutputSummary";
-import { pluralize } from "@forkara/shared/text";
+import { pluralize, stripTerminalControlSequences } from "@forkara/shared/text";
 import { PROVIDER_DESCRIPTORS } from "@forkara/shared/providerMetadata";
 import {
   deriveReadableToolTitle,
@@ -524,7 +524,7 @@ function toDerivedWorkLogEntry(activity: OrchestrationThreadActivity): DerivedWo
   const itemType = extractWorkLogItemType(payload);
   const requestKind = extractWorkLogRequestKind(payload);
   if (payload && typeof payload.detail === "string" && payload.detail.length > 0) {
-    const detail = stripTrailingExitCode(payload.detail).output;
+    const detail = stripTrailingExitCode(stripTerminalControlSequences(payload.detail)).output;
     if (detail) {
       entry.detail = detail;
     }
@@ -532,11 +532,11 @@ function toDerivedWorkLogEntry(activity: OrchestrationThreadActivity): DerivedWo
   const outputDetail =
     activity.kind === "provider.event.unmapped" ? null : summarizeToolPayloadOutput(payload);
   if (outputDetail && (!entry.detail || toolStatus === "failed")) {
-    entry.detail = outputDetail;
+    entry.detail = stripTerminalControlSequences(outputDetail);
   }
   const collabTaskOutputDetail = extractCollabTaskOutputDetail(payload);
   if (collabTaskOutputDetail) {
-    entry.detail = collabTaskOutputDetail;
+    entry.detail = stripTerminalControlSequences(collabTaskOutputDetail);
   }
   const nativeEventType =
     payload && typeof payload.nativeEventType === "string" && payload.nativeEventType.length > 0
@@ -549,7 +549,7 @@ function toDerivedWorkLogEntry(activity: OrchestrationThreadActivity): DerivedWo
     activity.kind === "runtime.warning" &&
     typeof payload?.message === "string" &&
     payload.message.trim().length > 0
-      ? payload.message.trim()
+      ? stripTerminalControlSequences(payload.message).trim()
       : undefined;
   if (runtimeWarningMessage) {
     entry.detail = runtimeWarningMessage;