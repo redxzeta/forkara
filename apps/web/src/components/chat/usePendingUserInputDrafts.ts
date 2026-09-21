Emanuele-web04/synara

Recover pending Claude questions across restarts and expired sessions (#1113)
* Fix expired Claude question recovery and duplicate submissions

* fix(web): preserve compiler coverage for question recovery

apps/web/src/components/chat/usePendingUserInputDrafts.ts
@@ -1,0 +1,71 @@
+import type { OrchestrationPendingInteraction, ThreadId } from "@synara/contracts";
+import { pendingRequestInstanceKey } from "@synara/shared/threadSummary";
+import { useCallback, useEffect, useLayoutEffect, useMemo, useRef } from "react";
+import { useComposerDraftStore, useComposerThreadDraft } from "../../composerDraftStore";
+import type { PendingUserInput } from "../../pendingInteractionDerivation";
+import type { PendingUserInputDraftAnswer } from "../../pendingUserInput";
+import type { PendingUserInputRecoveryDraft } from "../../pendingUserInputRecovery";
+
+type Answers = Record<string, Record<string, PendingUserInputDraftAnswer>>;
+const EMPTY_DRAFTS: Record<string, PendingUserInputRecoveryDraft> = {};
+
+function draftAnswers(drafts: Record<string, PendingUserInputRecoveryDraft>): Answers {
+  return Object.fromEntries(Object.entries(drafts).map(([key, draft]) => [key, draft.answers]));
+}
+
+export function usePendingUserInputDrafts(
+  threadId: ThreadId,
+  requests: ReadonlyArray<PendingUserInput>,
+  interactions: ReadonlyArray<OrchestrationPendingInteraction> | undefined,
+) {
+  const drafts = useComposerThreadDraft(threadId).pendingUserInputDrafts ?? EMPTY_DRAFTS;
+  const answers = useMemo(() => draftAnswers(drafts), [drafts]);
+  const answersRef = useRef<Answers>(answers);
+  const requestsRef = useRef(requests);
+  useLayoutEffect(() => {
+    answersRef.current = answers;
+    requestsRef.current = requests;
+  }, [answers, requests, threadId]);
+
+  const setAnswers = useCallback(
+    (update: (existing: Answers) => Answers) => {
+      const store = useComposerDraftStore.getState();
+      const existing = store.draftsByThreadId[threadId]?.pendingUserInputDrafts ?? EMPTY_DRAFTS;
+      const nextAnswers = update(draftAnswers(existing));
+      const nextDrafts = { ...existing };
+      for (const [key, requestAnswers] of Object.entries(nextAnswers)) {
+        const request =
+          existing[key]?.request ??
+          requestsRef.current.find(
+            (entry) =>
+              pendingRequestInstanceKey(entry.requestId, entry.lifecycleGeneration) === key,
+          );
+        if (request) nextDrafts[key] = { request, answers: requestAnswers };
+      }
+      answersRef.current = nextAnswers;
+      store.setPendingUserInputDrafts(threadId, nextDrafts);
+    },
+    [threadId],
+  );
+
+  // Command acceptance is not delivery. Keep the answer through transient
+  // failures and only discard it after authoritative settlement.
+  useEffect(() => {
+    const confirmed = new Set(
+      (interactions ?? [])
+        .filter((row) => row.interactionKind === "userInput" && row.status === "confirmed")
+        .map((row) =>
+          pendingRequestInstanceKey(row.requestId, row.lifecycleGeneration ?? undefined),
+        ),
+    );
+    if (!Object.keys(drafts).some((key) => confirmed.has(key))) return;
+    useComposerDraftStore
+      .getState()
+      .setPendingUserInputDrafts(
+        threadId,
+        Object.fromEntries(Object.entries(drafts).filter(([key]) => !confirmed.has(key))),
+      );
+  }, [drafts, interactions, threadId]);
+
+  return { answers, answersRef, setAnswers, drafts };
+}

apps/web/src/pendingUserInputRecovery.test.ts
@@ -1,0 +1,95 @@
+import { ApprovalRequestId, EventId, ThreadId } from "@synara/contracts";
+import {
+  buildStalePendingRequestFailureDetail,
+  pendingRequestInstanceKey,
+} from "@synara/shared/threadSummary";
+import { afterEach, expect, it } from "vitest";
+import {
+  expiredUserInputDrafts,
+  restoreUserInputDraft,
+  type PendingUserInputRecoveryDraft,
+} from "./pendingUserInputRecovery";
+import { partializeComposerDraftStoreState, useComposerDraftStore } from "./composerDraftStore";
+import {
+  normalizeCurrentPersistedComposerDraftStoreState,
+  toHydratedThreadDraft,
+} from "./composerDraftPersistence";
+
+const requestId = ApprovalRequestId.makeUnsafe("saved-question");
+const key = pendingRequestInstanceKey(requestId, "generation-a");
+const draft: PendingUserInputRecoveryDraft = {
+  request: {
+    requestId,
+    lifecycleGeneration: "generation-a",
+    createdAt: "2026-09-10T10:00:00.000Z",
+    questions: [
+      {
+        id: "1",
+        header: "Color",
+        question: "Which color?",
+        options: [{ label: "Blue", description: "Brand" }],
+      },
+      { id: "2", header: "Features", question: "Which features?", options: [], multiSelect: true },
+      { id: "3", header: "Notes", question: "Anything else?", options: [] },
+    ],
+  },
+  answers: {
+    "1": { selectedOptionLabels: ["Blue"] },
+    "2": { selectedOptionLabels: ["Search", "Tabs"] },
+    "3": { customAnswer: "Keep it simple" },
+  },
+};
+afterEach(() => useComposerDraftStore.setState({ draftsByThreadId: {} }));
+
+it("preserves structured answers without a normal prompt through persistence and reload", () => {
+  const threadId = ThreadId.makeUnsafe("saved-thread");
+  const store = useComposerDraftStore.getState();
+  store.setPendingUserInputDrafts(threadId, { [key]: draft });
+  store.setPrompt(threadId, "");
+  const persisted = partializeComposerDraftStoreState(useComposerDraftStore.getState());
+  const normalized = normalizeCurrentPersistedComposerDraftStoreState(
+    JSON.parse(JSON.stringify(persisted)),
+  );
+  const hydrated = toHydratedThreadDraft(threadId, normalized.draftsByThreadId[threadId]!);
+  expect(hydrated.pendingUserInputDrafts).toEqual({ [key]: draft });
+  store.clearComposerContent(threadId);
+  expect(
+    useComposerDraftStore.getState().draftsByThreadId[threadId]?.pendingUserInputDrafts,
+  ).toEqual({ [key]: draft });
+});
+
+it("only offers recovery for an explicit invalidation of the saved instance", () => {
+  const failure = {
+    id: EventId.makeUnsafe("expired"),
+    kind: "provider.user-input.respond.failed",
+    summary: "Failed",
+    tone: "error" as const,
+    createdAt: "2026-09-10T10:01:00.000Z",
+    turnId: null,
+    payload: {
+      requestId,
+      lifecycleGeneration: "generation-a",
+      detail: "temporary transport failure",
+    },
+  };
+  expect(expiredUserInputDrafts({ [key]: draft }, [failure])).toEqual([]);
+  const expired = {
+    ...failure,
+    payload: {
+      ...failure.payload,
+      detail: buildStalePendingRequestFailureDetail("user-input", requestId),
+    },
+  };
+  expect(expiredUserInputDrafts({ [key]: draft }, [expired])).toEqual([[key, draft]]);
+  expect(
+    expiredUserInputDrafts({ [key]: draft }, [
+      { ...expired, payload: { ...expired.payload, lifecycleGeneration: "different-generation" } },
+    ]),
+  ).toEqual([]);
+});
+
+it("restores all question-answer pairs after the existing normal draft", () => {
+  expect(restoreUserInputDraft("Existing draft", draft)).toBe(
+    "Existing draft\n\nWhich color?\nBlue\n\nWhich features?\nSearch, Tabs\n\nAnything else?\nKeep it simple",
+  );
+});

apps/web/src/pendingUserInputRecovery.ts
@@ -1,0 +1,87 @@
+import {
+  ApprovalRequestId,
+  UserInputQuestion,
+  type OrchestrationThreadActivity,
+} from "@synara/contracts";
+import { createStalePendingInteractionMatcher } from "@synara/shared/pendingInteractions";
+import { pendingRequestInstanceKey } from "@synara/shared/threadSummary";
+import { Schema } from "effect";
+import type { PendingUserInput } from "./pendingInteractionDerivation";
+import {
+  resolvePendingUserInputAnswer,
+  type PendingUserInputDraftAnswer,
+} from "./pendingUserInput";
+
+export interface PendingUserInputRecoveryDraft {
+  request: PendingUserInput;
+  answers: Record<string, PendingUserInputDraftAnswer>;
+}
+
+const StoredDraft = Schema.Struct({
+  request: Schema.Struct({
+    requestId: ApprovalRequestId,
+    lifecycleGeneration: Schema.optionalKey(Schema.String),
+    createdAt: Schema.String,
+    questions: Schema.Array(UserInputQuestion),
+  }),
+  answers: Schema.Record(
+    Schema.String,
+    Schema.Struct({
+      customAnswer: Schema.optionalKey(Schema.String),
+      selectedOptionLabels: Schema.optionalKey(Schema.Array(Schema.String)),
+    }),
+  ),
+});
+
+export function normalizePendingUserInputDrafts(
+  value: unknown,
+): Record<string, PendingUserInputRecoveryDraft> {
+  if (!value || typeof value !== "object" || Array.isArray(value)) return {};
+  return Object.fromEntries(
+    Object.entries(value).flatMap(([key, draft]) => {
+      if (
+        !Schema.is(StoredDraft)(draft) ||
+        key !==
+          pendingRequestInstanceKey(draft.request.requestId, draft.request.lifecycleGeneration)
+      ) {
+        return [];
+      }
+      const stored: typeof StoredDraft.Type = draft;
+      const answers = Object.fromEntries(
+        Object.entries(stored.answers).map(([id, answer]) => [
+          id,
+          {
+            ...(answer.customAnswer !== undefined ? { customAnswer: answer.customAnswer } : {}),
+            ...(answer.selectedOptionLabels
+              ? { selectedOptionLabels: [...answer.selectedOptionLabels] }
+              : {}),
+          },
+        ]),
+      );
+      return [[key, { request: stored.request, answers }]];
+    }),
+  );
+}
+
+export function expiredUserInputDrafts(
+  drafts: Record<string, PendingUserInputRecoveryDraft>,
+  activities: ReadonlyArray<OrchestrationThreadActivity>,
+): Array<[string, PendingUserInputRecoveryDraft]> {
+  const isStale = createStalePendingInteractionMatcher(activities);
+  return Object.entries(drafts).filter(([, draft]) =>
+    isStale({ ...draft.request, interactionKind: "userInput" }),
+  );
+}
+
+export function restoreUserInputDraft(
+  prompt: string,
+  draft: PendingUserInputRecoveryDraft,
+): string {
+  const pairs = draft.request.questions.flatMap((question) => {
+    const answer = resolvePendingUserInputAnswer(question, draft.answers[question.id]);
+    return answer === null
+      ? []
+      : [`${question.question}\n${Array.isArray(answer) ? answer.join(", ") : answer}`];
+  });
+  return [prompt, pairs.join("\n\n")].filter((part) => part.length > 0).join("\n\n");
+}

redxzeta/forkara

apps/web/src/components/chat/usePendingUserInputDrafts.ts
@@ -1,0 +1,71 @@
+import type { OrchestrationPendingInteraction, ThreadId } from "@forkara/contracts";
+import { pendingRequestInstanceKey } from "@forkara/shared/threadSummary";
+import { useCallback, useEffect, useLayoutEffect, useMemo, useRef } from "react";
+import { useComposerDraftStore, useComposerThreadDraft } from "../../composerDraftStore";
+import type { PendingUserInput } from "../../pendingInteractionDerivation";
+import type { PendingUserInputDraftAnswer } from "../../pendingUserInput";
+import type { PendingUserInputRecoveryDraft } from "../../pendingUserInputRecovery";
+
+type Answers = Record<string, Record<string, PendingUserInputDraftAnswer>>;
+const EMPTY_DRAFTS: Record<string, PendingUserInputRecoveryDraft> = {};
+
+function draftAnswers(drafts: Record<string, PendingUserInputRecoveryDraft>): Answers {
+  return Object.fromEntries(Object.entries(drafts).map(([key, draft]) => [key, draft.answers]));
+}
+
+export function usePendingUserInputDrafts(
+  threadId: ThreadId,
+  requests: ReadonlyArray<PendingUserInput>,
+  interactions: ReadonlyArray<OrchestrationPendingInteraction> | undefined,
+) {
+  const drafts = useComposerThreadDraft(threadId).pendingUserInputDrafts ?? EMPTY_DRAFTS;
+  const answers = useMemo(() => draftAnswers(drafts), [drafts]);
+  const answersRef = useRef<Answers>(answers);
+  const requestsRef = useRef(requests);
+  useLayoutEffect(() => {
+    answersRef.current = answers;
+    requestsRef.current = requests;
+  }, [answers, requests, threadId]);
+
+  const setAnswers = useCallback(
+    (update: (existing: Answers) => Answers) => {
+      const store = useComposerDraftStore.getState();
+      const existing = store.draftsByThreadId[threadId]?.pendingUserInputDrafts ?? EMPTY_DRAFTS;
+      const nextAnswers = update(draftAnswers(existing));
+      const nextDrafts = { ...existing };
+      for (const [key, requestAnswers] of Object.entries(nextAnswers)) {
+        const request =
+          existing[key]?.request ??
+          requestsRef.current.find(
+            (entry) =>
+              pendingRequestInstanceKey(entry.requestId, entry.lifecycleGeneration) === key,
+          );
+        if (request) nextDrafts[key] = { request, answers: requestAnswers };
+      }
+      answersRef.current = nextAnswers;
+      store.setPendingUserInputDrafts(threadId, nextDrafts);
+    },
+    [threadId],
+  );
+
+  // Command acceptance is not delivery. Keep the answer through transient
+  // failures and only discard it after authoritative settlement.
+  useEffect(() => {
+    const confirmed = new Set(
+      (interactions ?? [])
+        .filter((row) => row.interactionKind === "userInput" && row.status === "confirmed")
+        .map((row) =>
+          pendingRequestInstanceKey(row.requestId, row.lifecycleGeneration ?? undefined),
+        ),
+    );
+    if (!Object.keys(drafts).some((key) => confirmed.has(key))) return;
+    useComposerDraftStore
+      .getState()
+      .setPendingUserInputDrafts(
+        threadId,
+        Object.fromEntries(Object.entries(drafts).filter(([key]) => !confirmed.has(key))),
+      );
+  }, [drafts, interactions, threadId]);
+
+  return { answers, answersRef, setAnswers, drafts };
+}

apps/web/src/pendingUserInputRecovery.test.ts
@@ -1,0 +1,95 @@
+import { ApprovalRequestId, EventId, ThreadId } from "@forkara/contracts";
+import {
+  buildStalePendingRequestFailureDetail,
+  pendingRequestInstanceKey,
+} from "@forkara/shared/threadSummary";
+import { afterEach, expect, it } from "vitest";
+import {
+  expiredUserInputDrafts,
+  restoreUserInputDraft,
+  type PendingUserInputRecoveryDraft,
+} from "./pendingUserInputRecovery";
+import { partializeComposerDraftStoreState, useComposerDraftStore } from "./composerDraftStore";
+import {
+  normalizeCurrentPersistedComposerDraftStoreState,
+  toHydratedThreadDraft,
+} from "./composerDraftPersistence";
+
+const requestId = ApprovalRequestId.makeUnsafe("saved-question");
+const key = pendingRequestInstanceKey(requestId, "generation-a");
+const draft: PendingUserInputRecoveryDraft = {
+  request: {
+    requestId,
+    lifecycleGeneration: "generation-a",
+    createdAt: "2026-09-10T10:00:00.000Z",
+    questions: [
+      {
+        id: "1",
+        header: "Color",
+        question: "Which color?",
+        options: [{ label: "Blue", description: "Brand" }],
+      },
+      { id: "2", header: "Features", question: "Which features?", options: [], multiSelect: true },
+      { id: "3", header: "Notes", question: "Anything else?", options: [] },
+    ],
+  },
+  answers: {
+    "1": { selectedOptionLabels: ["Blue"] },
+    "2": { selectedOptionLabels: ["Search", "Tabs"] },
+    "3": { customAnswer: "Keep it simple" },
+  },
+};
+afterEach(() => useComposerDraftStore.setState({ draftsByThreadId: {} }));
+
+it("preserves structured answers without a normal prompt through persistence and reload", () => {
+  const threadId = ThreadId.makeUnsafe("saved-thread");
+  const store = useComposerDraftStore.getState();
+  store.setPendingUserInputDrafts(threadId, { [key]: draft });
+  store.setPrompt(threadId, "");
+  const persisted = partializeComposerDraftStoreState(useComposerDraftStore.getState());
+  const normalized = normalizeCurrentPersistedComposerDraftStoreState(
+    JSON.parse(JSON.stringify(persisted)),
+  );
+  const hydrated = toHydratedThreadDraft(threadId, normalized.draftsByThreadId[threadId]!);
+  expect(hydrated.pendingUserInputDrafts).toEqual({ [key]: draft });
+  store.clearComposerContent(threadId);
+  expect(
+    useComposerDraftStore.getState().draftsByThreadId[threadId]?.pendingUserInputDrafts,
+  ).toEqual({ [key]: draft });
+});
+
+it("only offers recovery for an explicit invalidation of the saved instance", () => {
+  const failure = {
+    id: EventId.makeUnsafe("expired"),
+    kind: "provider.user-input.respond.failed",
+    summary: "Failed",
+    tone: "error" as const,
+    createdAt: "2026-09-10T10:01:00.000Z",
+    turnId: null,
+    payload: {
+      requestId,
+      lifecycleGeneration: "generation-a",
+      detail: "temporary transport failure",
+    },
+  };
+  expect(expiredUserInputDrafts({ [key]: draft }, [failure])).toEqual([]);
+  const expired = {
+    ...failure,
+    payload: {
+      ...failure.payload,
+      detail: buildStalePendingRequestFailureDetail("user-input", requestId),
+    },
+  };
+  expect(expiredUserInputDrafts({ [key]: draft }, [expired])).toEqual([[key, draft]]);
+  expect(
+    expiredUserInputDrafts({ [key]: draft }, [
+      { ...expired, payload: { ...expired.payload, lifecycleGeneration: "different-generation" } },
+    ]),
+  ).toEqual([]);
+});
+
+it("restores all question-answer pairs after the existing normal draft", () => {
+  expect(restoreUserInputDraft("Existing draft", draft)).toBe(
+    "Existing draft\n\nWhich color?\nBlue\n\nWhich features?\nSearch, Tabs\n\nAnything else?\nKeep it simple",
+  );
+});

apps/web/src/pendingUserInputRecovery.ts
@@ -1,0 +1,87 @@
+import {
+  ApprovalRequestId,
+  UserInputQuestion,
+  type OrchestrationThreadActivity,
+} from "@forkara/contracts";
+import { createStalePendingInteractionMatcher } from "@forkara/shared/pendingInteractions";
+import { pendingRequestInstanceKey } from "@forkara/shared/threadSummary";
+import { Schema } from "effect";
+import type { PendingUserInput } from "./pendingInteractionDerivation";
+import {
+  resolvePendingUserInputAnswer,
+  type PendingUserInputDraftAnswer,
+} from "./pendingUserInput";
+
+export interface PendingUserInputRecoveryDraft {
+  request: PendingUserInput;
+  answers: Record<string, PendingUserInputDraftAnswer>;
+}
+
+const StoredDraft = Schema.Struct({
+  request: Schema.Struct({
+    requestId: ApprovalRequestId,
+    lifecycleGeneration: Schema.optionalKey(Schema.String),
+    createdAt: Schema.String,
+    questions: Schema.Array(UserInputQuestion),
+  }),
+  answers: Schema.Record(
+    Schema.String,
+    Schema.Struct({
+      customAnswer: Schema.optionalKey(Schema.String),
+      selectedOptionLabels: Schema.optionalKey(Schema.Array(Schema.String)),
+    }),
+  ),
+});
+
+export function normalizePendingUserInputDrafts(
+  value: unknown,
+): Record<string, PendingUserInputRecoveryDraft> {
+  if (!value || typeof value !== "object" || Array.isArray(value)) return {};
+  return Object.fromEntries(
+    Object.entries(value).flatMap(([key, draft]) => {
+      if (
+        !Schema.is(StoredDraft)(draft) ||
+        key !==
+          pendingRequestInstanceKey(draft.request.requestId, draft.request.lifecycleGeneration)
+      ) {
+        return [];
+      }
+      const stored: typeof StoredDraft.Type = draft;
+      const answers = Object.fromEntries(
+        Object.entries(stored.answers).map(([id, answer]) => [
+          id,
+          {
+            ...(answer.customAnswer !== undefined ? { customAnswer: answer.customAnswer } : {}),
+            ...(answer.selectedOptionLabels
+              ? { selectedOptionLabels: [...answer.selectedOptionLabels] }
+              : {}),
+          },
+        ]),
+      );
+      return [[key, { request: stored.request, answers }]];
+    }),
+  );
+}
+
+export function expiredUserInputDrafts(
+  drafts: Record<string, PendingUserInputRecoveryDraft>,
+  activities: ReadonlyArray<OrchestrationThreadActivity>,
+): Array<[string, PendingUserInputRecoveryDraft]> {
+  const isStale = createStalePendingInteractionMatcher(activities);
+  return Object.entries(drafts).filter(([, draft]) =>
+    isStale({ ...draft.request, interactionKind: "userInput" }),
+  );
+}
+
+export function restoreUserInputDraft(
+  prompt: string,
+  draft: PendingUserInputRecoveryDraft,
+): string {
+  const pairs = draft.request.questions.flatMap((question) => {
+    const answer = resolvePendingUserInputAnswer(question, draft.answers[question.id]);
+    return answer === null
+      ? []
+      : [`${question.question}\n${Array.isArray(answer) ? answer.join(", ") : answer}`];
+  });
+  return [prompt, pairs.join("\n\n")].filter((part) => part.length > 0).join("\n\n");
+}